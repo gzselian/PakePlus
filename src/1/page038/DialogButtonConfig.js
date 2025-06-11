d47bacc8282eba123ddb3a1aec5188296c5bf629
gzselian <3441775508@qq.com>
2025-06-11 14:55:19 +0800

    update src file from pakeplus

---
+++ src/1/page038/DialogButtonConfig.js
@@ -0,0 +1,73 @@
+var page038 = {
+    "Success": true,
+    "data": {
+        "pageContents": [
+			{
+			    "contentId": 8888,
+			    "sortNum": "97",
+			    "x": "151.55",
+			    "y": "37",
+			    "width": "206",
+			    "height": "38",
+			    "audio": "source/title/Communicate.mp3"
+			},
+			{
+                "contentId": 8888,
+                "sortNum": "1",
+                "x": "59.95",
+                "y": "111.9",
+                "width": "322",
+                "height": "32",
+                "audio": "source/title/Listen and number then say.mp3"
+            }
+        ],
+        "buttons": [
+            {
+                "id": "1",
+                "sourceType": "11",
+                "x": "385.75",
+                "y": "109.9",
+                "width": "33",
+                "height": "33",
+                "fileUrl": "source/sound/p38 A Listen and number_Then say.mp3",
+                "breakpage": "",
+                "startpage": 0,
+                "endpage": 0,
+                "startindex": 0,
+                "endindex": 0,
+                "title": "课文录音"
+            },
+            {
+                "id": "2",
+                "sourceType": "93",
+                "x": "439",
+                "y": "109.9",
+                "width": "33",
+                "height": "33",
+                "fileUrl": "source/h5/u4_p38 A Listen and number_Then say02/index.html",
+                "breakpage": "",
+                "startpage": 0,
+                "endpage": 0,
+                "startindex": 0,
+                "endindex": 0,
+                "title": "课件"
+            },
+            {
+                "id": "3",
+                "sourceType": "94",
+                "x": "409.75",
+                "y": "109.9",
+                "width": "33",
+                "height": "33",
+                "fileUrl": "source/h5/u4_p38 A Listen and number_Then say/index.html",
+                "breakpage": "",
+                "startpage": 0,
+                "endpage": 0,
+                "startindex": 0,
+                "endindex": 0,
+                "title": "练习"
+            }
+        ]
+    },
+    "Message": ""
+};
