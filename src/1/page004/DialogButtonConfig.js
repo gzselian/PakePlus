313f3a26e75ebb3326bb9bf08ca1850f15da4ee1
gzselian <3441775508@qq.com>
2025-06-11 14:47:59 +0800

    update src file from pakeplus

---
+++ src/1/page004/DialogButtonConfig.js
@@ -0,0 +1,194 @@
+var page004 = {
+	"Success": true,
+	"data": {
+		"pageContents": [{
+				"contentId": 8888,
+				"sortNum": "98",
+				"x": "160.4",
+				"y": "38.35",
+				"width": "120",
+				"height": "36",
+				"audio": "source/title/Explore.mp3"
+			},
+			{
+				"contentId": 8888,
+				"sortNum": "1",
+				"x": "104.7",
+				"y": "124.95",
+				"width": "262",
+				"height": "58.05",
+				"audio": "source/sound/p4 Explore 00.mp3"
+			},
+			{
+				"contentId": 8888,
+				"sortNum": "2",
+				"x": "104.7",
+				"y": "183",
+				"width": "484.95",
+				"height": "58.05",
+				"audio": "source/sound/p4 Explore 00.mp3"
+			}, {
+				"contentId": 8888,
+				"sortNum": "1",
+				"x": "300.85",
+				"y": "313.05",
+				"width": "142",
+				"height": "27.95",
+				"audio": "source/sound/P4 Explore 01.mp3"
+			},
+			{
+				"contentId": 8888,
+				"sortNum": "2",
+				"x": "441.45",
+				"y": "313.05",
+				"width": "120",
+				"height": "27.95",
+				"audio": "source/sound/P4 Explore 02.mp3"
+			},
+			{
+				"contentId": 8888,
+				"sortNum": "2",
+				"x": "296.85",
+				"y": "341.05",
+				"width": "109",
+				"height": "27.95",
+				"audio": "source/sound/P4 Explore 02.mp3"
+			},
+			{
+				"contentId": 8888,
+				"sortNum": "3",
+				"x": "405.85",
+				"y": "341.05",
+				"width": "149.95",
+				"height": "27.95",
+				"audio": "source/sound/P4 Explore 03.mp3"
+			},
+			{
+				"contentId": 8888,
+				"sortNum": "3",
+				"x": "296.4",
+				"y": "367",
+				"width": "86",
+				"height": "27.95",
+				"audio": "source/sound/P4 Explore 03.mp3"
+			},
+			{
+				"contentId": 8888,
+				"sortNum": "4",
+				"x": "381.3",
+				"y": "367",
+				"width": "168.95",
+				"height": "27.95",
+				"audio": "source/sound/P4 Explore 04.mp3"
+			},
+			{
+				"contentId": 8888,
+				"sortNum": "4",
+				"x": "296.6",
+				"y": "393.75",
+				"width": "227",
+				"height": "27.95",
+				"audio": "source/sound/P4 Explore 04.mp3"
+			},
+			{
+				"contentId": 8888,
+				"sortNum": "5",
+				"x": "520.25",
+				"y": "389.2",
+				"width": "56",
+				"height": "27.95",
+				"audio": "source/sound/P4 Explore 05.mp3"
+			},
+			{
+				"contentId": 8888,
+				"sortNum": "5",
+				"x": "300.55",
+				"y": "416.2",
+				"width": "151",
+				"height": "27.95",
+				"audio": "source/sound/P4 Explore 05.mp3"
+			},
+			{
+				"contentId": 8888,
+				"sortNum": "6",
+				"x": "95.8",
+				"y": "728.8",
+				"width": "112",
+				"height": "27.95",
+				"audio": "source/sound/P4 Explore 06.mp3"
+			},
+			{
+				"contentId": 8888,
+				"sortNum": "7",
+				"x": "93.7",
+				"y": "756.75",
+				"width": "245",
+				"height": "27.95",
+				"audio": "source/sound/P4 Explore 07.mp3"
+			},
+			{
+				"contentId": 8888,
+				"sortNum": "8",
+				"x": "93.45",
+				"y": "783.7",
+				"width": "266",
+				"height": "27.95",
+				"audio": "source/sound/P4 Explore 08.mp3"
+			},
+			{
+				"contentId": 8888,
+				"sortNum": "9",
+				"x": "92.7",
+				"y": "811.65",
+				"width": "230",
+				"height": "27.95",
+				"audio": "source/sound/P4 Explore 09.mp3"
+			}
+		],
+		"buttons": [{
+				"id": "1",
+				"sourceType": "11",
+				"x": "288.8",
+				"y": "37.65",
+				"width": "33",
+				"height": "33",
+				"fileUrl": "source/sound/p4 Explore.mp3",
+				"breakpage": "",
+				"startpage": 0,
+				"endpage": 0,
+				"startindex": 0,
+				"endindex": 0,
+				"title": "课文录音"
+			},
+			{
+				"id": "2",
+				"sourceType": "14",
+				"x": "318.8",
+				"y": "37.65",
+				"width": "33",
+				"height": "33",
+				"fileUrl": "",
+				"startpage": 4,
+				"endpage": 5,
+				"startindex": 2,
+				"endindex": 11,
+				"title": "跟读"
+			},
+			{
+				"id": "3",
+				"sourceType": "92",
+				"x": "348.8",
+				"y": "37.65",
+				"width": "33",
+				"height": "33",
+				"fileUrl": "source/h5/u1_p4 Explore/index.html",
+				"startpage": 2,
+				"endpage": 2,
+				"startindex": 1,
+				"endindex": 2,
+				"title": "动画"
+			}
+		],
+	},
+	"Message": ""
+};
