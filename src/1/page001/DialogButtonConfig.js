cb91aff6e5ab46e2cc355bf5156d6d30db2e94aa
gzselian <3441775508@qq.com>
2025-06-11 14:47:01 +0800

    update src file from pakeplus

---
+++ src/1/page001/DialogButtonConfig.js
@@ -0,0 +1,43 @@
+var page001 = {
+    "Success": true,
+    "data": {
+        "pageContents": [
+            {
+                "contentId": 8888,
+                "sortNum": "1",
+                "x": "164.05",
+                "y": "112.85",
+                "width": "381.95",
+                "height": "48.85",
+                "audio": "source/sound/p1 How do we spend our free time.mp3"
+            },
+            {
+                "contentId": 8888,
+                "sortNum": "2",
+                "x": "345.3",
+                "y": "163.45",
+                "width": "199",
+                "height": "41",
+                "audio": "source/sound/p1 How do we spend our free time.mp3"
+            }
+        ],
+        "buttons": [
+		{
+				"id": "0",
+				"sourceType": "92",
+				"x": "680",
+				"y": "920",
+				"width": "60",
+				"height": "60",
+				"fileUrl": "source/mov/daor/u1_daor.mp4",
+				// "breakpage":"33,6",
+				"startpage": 0,
+				"endpage": 0,
+				"startindex": 0,
+				"endindex": 0,
+				"title": "导入视频"
+			}
+        ]
+    },
+    "Message": ""
+};
