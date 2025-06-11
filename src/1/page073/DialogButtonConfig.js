e116646dde881a25330a8de45c9f58726891fdb6
gzselian <3441775508@qq.com>
2025-06-11 15:02:03 +0800

    update src file from pakeplus

---
+++ src/1/page073/DialogButtonConfig.js
@@ -0,0 +1,65 @@
+
+var page073 = {
+    "Success": true,
+    "data": {
+        "pageContents": [
+		{
+                "contentId": 8888,
+                "sortNum": "1",
+                "x": "75.65",
+                "y": "199.95",
+                "width": "226",
+                "height": "36",
+                "audio": "source/title/p73 Listen choose and say.mp3"
+            }
+        ],
+        "buttons": [
+            {
+                "id": "1",
+                "sourceType": "11",
+                "x": "302.75",
+                "y": "199.9",
+                "width": "33",
+                "height": "33",
+                "fileUrl": "source/sound/p73 Listen_choose and say.mp3",
+                "breakpage": "",
+                "startpage": 0,
+                "endpage": 0,
+                "startindex": 0,
+                "endindex": 0,
+                "title": "课文录音"
+            },
+            {
+                "id": "2",
+                "sourceType": "93",
+                "x": "358.75",
+                "y": "199.9",
+                "width": "33",
+                "height": "33",
+                "fileUrl": "source/h5/u8_p73 C Listen_choose and say02/index.html",
+                "breakpage": "",
+                "startpage": 0,
+                "endpage": 0,
+                "startindex": 0,
+                "endindex": 0,
+                "title": "课件"
+            },
+            {
+                "id": "3",
+                "sourceType": "94",
+                "x": "330.75",
+                "y": "199.9",
+                "width": "33",
+                "height": "33",
+                "fileUrl": "source/h5/u8_p73 C Listen_choose and say/index.html",
+                "breakpage": "",
+                "startpage": 0,
+                "endpage": 0,
+                "startindex": 0,
+                "endindex": 0,
+                "title": "练习"
+            }
+        ]
+    },
+    "Message": ""
+};
