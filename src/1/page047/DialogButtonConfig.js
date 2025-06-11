8afce8b6ba868e66d99b9b378701de955ce53ccd
gzselian <3441775508@qq.com>
2025-06-11 14:57:02 +0800

    update src file from pakeplus

---
+++ src/1/page047/DialogButtonConfig.js
@@ -0,0 +1,197 @@
+
+var page047 = {
+    "Success": true,
+    "data": {
+        "pageContents": [
+            {
+                "contentId": 8888,
+                "sortNum": "1",
+                "x": "190.25",
+                "y": "106.9",
+                "width": "370",
+                "height": "30",
+                "audio": "source/sound/p47 Listen and chant 01.mp3"
+            },
+            {
+                "contentId": 8888,
+                "sortNum": "2",
+                "x": "68.25",
+                "y": "142.9",
+                "width": "327.95",
+                "height": "30",
+                "audio": "source/sound/p47 Listen and chant 02.mp3"
+            },
+            {
+                "contentId": 8888,
+                "sortNum": "3",
+                "x": "67.25",
+                "y": "172.9",
+                "width": "284",
+                "height": "30",
+                "audio": "source/sound/p47 Listen and chant 03.mp3"
+            },
+            {
+                "contentId": 8888,
+                "sortNum": "4",
+                "x": "67.1",
+                "y": "202.9",
+                "width": "265",
+                "height": "30",
+                "audio": "source/sound/p47 Listen and chant 04.mp3"
+            },
+            {
+                "contentId": 8888,
+                "sortNum": "5",
+                "x": "67.1",
+                "y": "232.9",
+                "width": "265",
+                "height": "30",
+                "audio": "source/sound/p47 Listen and chant 05.mp3"
+            },
+            {
+                "contentId": 8888,
+                "sortNum": "6",
+                "x": "68.25",
+                "y": "264.9",
+                "width": "327.95",
+                "height": "30",
+                "audio": "source/sound/p47 Listen and chant 06.mp3"
+            },
+            {
+                "contentId": 8888,
+                "sortNum": "7",
+                "x": "67.1",
+                "y": "295.9",
+                "width": "265",
+                "height": "30",
+                "audio": "source/sound/p47 Listen and chant 07.mp3"
+            },
+            {
+                "contentId": 8888,
+                "sortNum": "8",
+                "x": "67.1",
+                "y": "325.9",
+                "width": "265",
+                "height": "30",
+                "audio": "source/sound/p47 Listen and chant 08.mp3"
+            },
+            {
+                "contentId": 8888,
+                "sortNum": "9",
+                "x": "68.25",
+                "y": "355.9",
+                "width": "327.95",
+                "height": "30",
+                "audio": "source/sound/p47 Listen and chant 09.mp3"
+            },
+            {
+                "contentId": 8888,
+                "sortNum": "10",
+                "x": "98",
+                "y": "490.9",
+                "width": "196",
+                "height": "30",
+                "audio": "source/sound/p47 Learn 01.mp3"
+            },
+            {
+                "contentId": 8888,
+                "sortNum": "11",
+                "x": "95.3",
+                "y": "520.9",
+                "width": "255",
+                "height": "30",
+                "audio": "source/sound/p47 Learn 02.mp3"
+            },
+            {
+                "contentId": 8888,
+                "sortNum": "12",
+                
+				"x": "509.5",
+				"y": "502.7",
+				"width": "174",
+				"height": "34",
+                "audio": "source/sound/p47 Learn 03.mp3"
+            },{
+                "contentId": 8888,
+                "sortNum": "1",
+                "x": "55.65",
+                "y": "70.95",
+                "width": "206",
+                "height": "36",
+                "audio": "source/title/C Listen and chant.mp3"
+            },
+			{
+                "contentId": 8888,
+                "sortNum": "1",
+                "x": "128.05",
+                "y": "452.85",
+                "width": "66",
+                "height": "32",
+                "audio": "source/title/Learn.mp3"
+            }
+        ],
+        "buttons": [
+            {
+                "id": "1",
+                "sourceType": "11",
+                "x": "259.75",
+                "y": "71.9",
+                "width": "33",
+                "height": "33",
+                "fileUrl": "source/sound/p47 Listen and chant.mp3",
+                "breakpage": "",
+                "startpage": 0,
+                "endpage": 0,
+                "startindex": 0,
+                "endindex": 0,
+                "title": "课文录音"
+            },
+            {
+                "id": "2",
+                "sourceType": "93",
+                "x": "291.75",
+                "y": "71.9",
+                "width": "33",
+                "height": "33",
+                "fileUrl": "source/h5/u5_p47 C Listen and chant/index.html",
+                "breakpage": "",
+                "startpage": 0,
+                "endpage": 0,
+                "startindex": 0,
+                "endindex": 0,
+                "title": "课件"
+            },
+            {
+                "id": "3",
+                "sourceType": "93",
+                "x": "235.4",
+                "y": "599.85",
+                "width": "33",
+                "height": "33",
+                "fileUrl": "source/h5/u5_p47 D Point and say/index.html",
+                "breakpage": "",
+                "startpage": 0,
+                "endpage": 0,
+                "startindex": 0,
+                "endindex": 0,
+                "title": "课件"
+            },
+            {
+                "id": "4",
+                "sourceType": "93",
+                "x": "202.4",
+                "y": "452.85",
+                "width": "33",
+                "height": "33",
+                "fileUrl": "source/h5/u5_p47 Learn/index.html",
+                "breakpage": "",
+                "startpage": 0,
+                "endpage": 0,
+                "startindex": 0,
+                "endindex": 0,
+                "title": "课件"
+            }
+        ]
+    },
+    "Message": ""
+};
