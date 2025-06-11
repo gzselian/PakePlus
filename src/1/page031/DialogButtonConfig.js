6ae2b0aa0f42ada7dda10520bdae7a7c70615854
gzselian <3441775508@qq.com>
2025-06-11 14:54:02 +0800

    update src file from pakeplus

---
+++ src/1/page031/DialogButtonConfig.js
@@ -0,0 +1,35 @@
+
+var page031 = {
+    "Success": true,
+    "data": {
+        "pageContents": [
+            {
+                "contentId": 8888,
+                "sortNum": "1",
+                "x": "91",
+                "y": "113.05",
+                "width": "442",
+                "height": "54",
+                "audio": "source/sound/p31 What sounds can we hear.mp3"
+            }
+        ],
+        "buttons": [
+		{
+				"id": "0",
+				"sourceType": "92",
+				"x": "680",
+				"y": "890",
+				"width": "60",
+				"height": "60",
+				"fileUrl": "source/mov/daor/u4_daor.mp4",
+				// "breakpage":"33,6",
+				"startpage": 0,
+				"endpage": 0,
+				"startindex": 0,
+				"endindex": 0,
+				"title": "导入视频"
+			}
+        ]
+    },
+    "Message": ""
+};
