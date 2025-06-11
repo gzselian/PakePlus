5f28f588cf4c4fae9c0ee4b687ff438a54fbbe6b
gzselian <3441775508@qq.com>
2025-06-11 15:48:02 +0800

    update src file from pakeplus

---
+++ src/1/source/h5/u3_p24 Explore/js/subtitleDB.js
@@ -0,0 +1,74 @@
+var movPath="../../mov/P24 Explore.mp4";
+var soundPath="source/01bg.mp3";
+var movRatio="16:9";
+
+var subtitleJson = {
+"wordChArr": [
+    "裤子、短裤、短裙······",
+	"你在学校穿什么？",
+	"世界各地的学生穿不同的衣服。",
+	"让我们来看一看。",
+	"吴晨是一名三年级的中国学生。",
+	"在我的学校，我们穿校服。",
+	"我穿衬衫和短裙。",
+	"在晴天，我会戴帽子。",
+	"— 吴晨",
+	"加里是一名三年级的澳大利亚学生。",
+	"在我的学校，我们不穿校服。",
+	"在阴天，我穿T恤和裤子。",
+	"但在晴天，我穿短裤。",
+	"当然，我也会戴帽子！",
+	"— 加里"
+],
+"wordArr": [
+    "Trousers, shorts, a skirt ...",
+    "What do you wear at school?",
+    "Students around the world wear different clothes.",
+    "Let’s take a look.",
+    "Wu Chen is a Chinese student in Grade 3.",
+    "At my school, we wear a uniform.",
+    "I wear a shirt and a skirt.",
+    "On a sunny day, I wear a cap.",
+    "— Wu Chen",
+    "Gary is an Australian student in Grade 3.",
+    "At my school, we don’t wear a uniform. ",
+    "On a cloudy day, I wear a T-shirt and trousers.",
+    "But on a sunny day, I wear shorts.",
+    "And, of course, I wear a cap too!",
+	"— Gary"
+	],
+
+"timeArr": [
+"5666", "9933",
+"10200", "13300",
+"13766", "18833",
+"19000", "21533",
+"22000", "27633",
+"28000", "32666",
+"33000", "37266",
+"37766", "42100",
+"42400", "44066",
+"44733", "50000",
+"50666", "55333",
+"55633", "61900",
+"62300", "66766",
+"67066", "72333",
+"72800", "73966"
+], // 15
+// 齐声
+// 1:05.349-1:07.919
+// 角色出现顺序
+"rolepoints": [
+    "t", "t", "t", "t", "t", 
+	"a","a", "a", "a", 
+	"t",
+	"b", "b","b", "b", "b"
+],
+"allrole":
+     ["t","a","b"]
+};
+var rolelist=["t","a","b"];
+
+var Allrolepoints = [];
+
+ 
