d62c51d895249da9af0c45ce34b424a5fe0d66f0
gzselian <3441775508@qq.com>
2025-06-11 14:59:43 +0800

    update src file from pakeplus

---
+++ src/1/page060/DialogButtonConfig.js
@@ -0,0 +1,261 @@
+var page060 = {
+	"Success": true,
+	"data": {
+		"pageContents": [
+		    {
+				"contentId": 8888,
+				"sortNum": "96",
+				"x": "163.75",
+				"y": "36.75",
+				"width": "112",
+				"height": "38",
+				"audio": "source/title/Extend.mp3"
+			},
+			{
+                "contentId": 8888,
+                "sortNum": "1",
+                "x": "190.3",
+                "y": "109.7",
+                "width": "330.95",
+                "height": "76",
+                "audio": "source/sound/p60 Extend 01.mp3"
+            },
+            {
+                "contentId": 8888,
+                "sortNum": "2",
+                "x": "136.5",
+                "y": "210.65",
+                "width": "471",
+                "height": "28",
+                "audio": "source/sound/p60 Extend 02.mp3"
+            },
+            {
+                "contentId": 8888,
+                "sortNum": "3",
+                "x": "135",
+                "y": "238.65",
+                "width": "294",
+                "height": "28",
+                "audio": "source/sound/p60 Extend 03.mp3"
+            },
+            {
+                "contentId": 8888,
+                "sortNum": "4",
+                "x": "40.75",
+                "y": "486.2",
+                "width": "182",
+                "height": "32",
+                "audio": "source/sound/p60 Extend 04.mp3"
+            },
+            {
+                "contentId": 8888,
+                "sortNum": "5",
+                "x": "221.35",
+                "y": "486.2",
+                "width": "115",
+                "height": "32",
+                "audio": "source/sound/p60 Extend 05.mp3"
+            },
+            {
+                "contentId": 8888,
+                "sortNum": "5",
+                "x": "182.5",
+                "y": "518.2",
+                "width": "134",
+                "height": "32",
+                "audio": "source/sound/p60 Extend 05.mp3"
+            },
+            {
+                "contentId": 8888,
+                "sortNum": "5",
+                "x": "40.7",
+                "y": "518.2",
+                "width": "143",
+                "height": "32",
+                "audio": "source/sound/p60 Extend 05.mp3"
+            },
+            {
+                "contentId": 8888,
+                "sortNum": "6",
+                "x": "378",
+                "y": "486.15",
+                "width": "190",
+                "height": "32",
+                "audio": "source/sound/p60 Extend 06.mp3"
+            },
+            {
+                "contentId": 8888,
+                "sortNum": "7",
+                "x": "566.55",
+                "y": "486.15",
+                "width": "101",
+                "height": "32",
+                "audio": "source/sound/p60 Extend 07.mp3"
+            },
+            {
+                "contentId": 8888,
+                "sortNum": "7",
+                "x": "375.5",
+                "y": "518.4",
+                "width": "197",
+                "height": "26",
+                "audio": "source/sound/p60 Extend 07.mp3"
+            },
+            {
+                "contentId": 8888,
+                "sortNum": "8",
+                "x": "570.7",
+                "y": "518.4",
+                "width": "118",
+                "height": "26",
+                "audio": "source/sound/p60 Extend 08.mp3"
+            },
+            {
+                "contentId": 8888,
+                "sortNum": "8",
+                "x": "378.8",
+                "y": "544.4",
+                "width": "79",
+                "height": "26",
+                "audio": "source/sound/p60 Extend 08.mp3"
+            },
+            {
+                "contentId": 8888,
+                "sortNum": "9",
+                "x": "45.35",
+                "y": "796.6",
+                "width": "157",
+                "height": "30",
+                "audio": "source/sound/p60 Extend 09.mp3"
+            },
+            {
+                "contentId": 8888,
+                "sortNum": "10",
+                "x": "202.3",
+                "y": "796.6",
+                "width": "150",
+                "height": "30",
+                "audio": "source/sound/p60 Extend 10.mp3"
+            },
+            {
+                "contentId": 8888,
+                "sortNum": "10",
+                "x": "44.8",
+                "y": "826.6",
+                "width": "179",
+                "height": "30",
+                "audio": "source/sound/p60 Extend 10.mp3"
+            },
+            {
+                "contentId": 8888,
+                "sortNum": "11",
+                "x": "220.5",
+                "y": "826.6",
+                "width": "120",
+                "height": "30",
+                "audio": "source/sound/p60 Extend 11.mp3"
+            },
+            {
+                "contentId": 8888,
+                "sortNum": "11",
+                "x": "44.1",
+                "y": "850.25",
+                "width": "161",
+                "height": "30",
+                "audio": "source/sound/p60 Extend 11.mp3"
+            },
+            {
+                "contentId": 8888,
+                "sortNum": "12",
+                "x": "377.9",
+                "y": "796.6",
+                "width": "159",
+                "height": "32",
+                "audio": "source/sound/p60 Extend 12.mp3"
+            },
+            {
+                "contentId": 8888,
+                "sortNum": "13",
+                "x": "536.25",
+                "y": "796.6",
+                "width": "144",
+                "height": "32",
+                "audio": "source/sound/p60 Extend 13.mp3"
+            },
+            {
+                "contentId": 8888,
+                "sortNum": "13",
+                "x": "380.75",
+                "y": "823.6",
+                "width": "116",
+                "height": "32",
+                "audio": "source/sound/p60 Extend 13.mp3"
+            },
+            {
+                "contentId": 8888,
+                "sortNum": "14",
+                "x": "494.3",
+                "y": "823.6",
+                "width": "173",
+                "height": "32",
+                "audio": "source/sound/p60 Extend 14.mp3"
+            },
+            {
+                "contentId": 8888,
+                "sortNum": "15",
+                "x": "379.35",
+                "y": "850.25",
+                "width": "142",
+                "height": "28",
+                "audio": "source/sound/p60 Extend 15.mp3"
+            }
+			
+		],
+		"buttons": [{
+				"id": "1",
+				"sourceType": "11",
+				"x": "283.25",
+				"y": "38.9",
+				"width": "33",
+				"height": "33",
+				"fileUrl": "source/sound/p60 Extend.mp3",
+				"breakpage": "",
+				"startpage": 0,
+				"endpage": 0,
+				"startindex": 0,
+				"endindex": 0,
+				"title": "课文录音"
+			},
+			{
+				"id": "2",
+				"sourceType": "14",
+			    "x": "316.25",
+			    "y": "38.9",
+				"width": "33",
+				"height": "33",
+				"breakpage": "",
+				"startpage": 60,
+				"endpage": 60,
+				"startindex": 2,
+				"endindex": 24,
+				"title": "跟读"
+			},
+			{
+				"id": "3",
+				"sourceType": "92",
+				"x": "347.25",
+				"y": "38.9",
+				"width": "33",
+				"height": "33",
+				"fileUrl": "source/h5/u6_p60 Extend/index.html",
+				"breakpage": "",
+				"startpage": 0,
+				"endpage": 0,
+				"startindex": 0,
+				"endindex": 0,
+				"title": "动画"
+			}
+		]
+	},
+	"Message": ""
+};
