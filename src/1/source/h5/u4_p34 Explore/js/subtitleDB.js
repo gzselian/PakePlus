0a46d762f23564610861bd43049b293afaf4880a
gzselian <3441775508@qq.com>
2025-06-11 16:05:53 +0800

    update src file from pakeplus

---
+++ src/1/source/h5/u4_p34 Explore/js/subtitleDB.js
@@ -0,0 +1,119 @@
+var movPath="../../mov/P34 Explore.mp4";
+var soundPath="source/01bg.mp3";
+var movRatio="16:9";
+
+var subtitleJson = {
+
+"wordChArr": [
+    "我们到户外去吧——",
+    "只有你和我！",
+    "你能听到什么？",
+    "你能看到什么？",
+	
+    "唧唧，唧唧！",
+    "一只漂亮的鸟在树上。",
+    "嗡嗡，嗡嗡！",
+    "一只黑黄相间的蜜蜂。",
+	
+    "沿着河边走——",
+    "只有你和我！",
+    "你能听到什么？",
+    "你能看到什么？",
+	
+    "嗖嗖，嗖嗖！",
+    "风吹着草。",
+    "嘎嘎，嘎嘎！",
+    "一只鸭子游了过去。",
+	
+    "沿着小路走——",
+    "只有你和我！",
+    "你能听到什么？",
+    "你能看到什么？",
+	
+    "淅淅沥沥！",
+    "我能听到雨声。",
+    "我想是时候",
+    "再次回家了"
+],
+"wordArr": [
+    "Let’s go outside —",
+    "Just you and me!",
+    "What can you hear?",
+    "What can you see?",
+	
+    "Chirp, chirp!",
+    "A nice bird in a tree.",
+    "Buzz, buzz!",
+    "A black and yellow bee.",
+	
+    "Down by the river —",
+    "Just you and me!",
+    "What can you hear?",
+    "What can you see?",
+	
+    "Swish, swish!",
+    "The wind blows the grass.",
+    "Quack, quack!",
+    "A duck swims past.",
+	
+    "Walk along the path —",
+    "Just you and me!",
+    "What can you hear?",
+    "What can you see?",
+	
+    "Pitter-patter!",
+    "I can hear the rain.",
+    "I think it’s time ",
+    "To go home again!"
+],
+"timeArr": [
+"5366", "8133",
+"8533", "11100",
+"11433", "14100",
+"14333", "17066",
+"21166", "23200",
+
+"23600", "26933",
+"29833", "31900",
+"32033", "35066",
+"36500", "39000",
+"39100", "42033",
+
+"42233", "44833",
+"45066", "47766",
+"51833", "54433",
+"55500", "58333",
+"59100", "60800",
+
+"61500", "64033",
+"67200", "69800",
+"69933", "72400",
+"72766", "75200",
+"75400", "77900",
+
+"83466", "85200",
+"85300", "87933",
+"88233", "90800",
+"90833", "93266"
+], // 24
+"rolepoints": [
+    "b", "b", "b", "b", 
+	"g", "g","g", "g",
+	"b","b", "b", "b",
+	"g","g", "g", "g", 
+	"b","b","b", "b",
+	"g", "g", "g", "g"
+],
+
+// 齐声
+// 1:05.349-1:07.919
+// 角色出现顺序
+
+"allrole":
+     ["b","g"]
+};
+var rolelist=["b","g"];
+
+var Allrolepoints = [];
+
+ 
