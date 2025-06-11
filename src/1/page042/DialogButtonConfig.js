cbf05118e18b4fcdfc7da776f5019496f3025b8b
gzselian <3441775508@qq.com>
2025-06-11 14:56:07 +0800

    update src file from pakeplus

---
+++ src/1/page042/DialogButtonConfig.js
@@ -0,0 +1,148 @@
+
+var page042 = {
+    "Success": true,
+    "data": {
+        "pageContents": [
+            {
+                "contentId": 8888,
+                "sortNum": "1",
+                "x": "101.6",
+                "y": "203.65",
+                "width": "81",
+                "height": "36",
+                "audio": "source/sound/pink.mp3"
+            },
+            {
+                "contentId": 8888,
+                "sortNum": "2",
+                "x": "87.6",
+                "y": "267.65",
+                "width": "81",
+                "height": "36",
+                "audio": "source/sound/blue.mp3"
+            },
+            {
+                "contentId": 8888,
+                "sortNum": "3",
+                "x": "83.6",
+                "y": "342.6",
+                "width": "81",
+                "height": "36",
+                "audio": "source/sound/red.mp3"
+            },
+            {
+                "contentId": 8888,
+                "sortNum": "4",
+                "x": "101.6",
+                "y": "417.6",
+                "width": "81",
+                "height": "36",
+                "audio": "source/sound/brown.mp3"
+            },
+            {
+                "contentId": 8888,
+                "sortNum": "5",
+                "x": "137.6",
+                "y": "484.55",
+                "width": "81",
+                "height": "36",
+                "audio": "source/sound/black.mp3"
+            },
+            {
+                "contentId": 8888,
+                "sortNum": "6",
+                "x": "547.6",
+                "y": "203.65",
+                "width": "81",
+                "height": "36",
+                "audio": "source/sound/white.mp3"
+            },
+            {
+                "contentId": 8888,
+                "sortNum": "7",
+                "x": "578.6",
+                "y": "267.65",
+                "width": "81",
+                "height": "36",
+                "audio": "source/sound/purple.mp3"
+            },
+            {
+                "contentId": 8888,
+                "sortNum": "8",
+                "x": "578.6",
+                "y": "342.6",
+                "width": "81",
+                "height": "36",
+                "audio": "source/sound/green.mp3"
+            },
+            {
+                "contentId": 8888,
+                "sortNum": "9",
+                "x": "552.6",
+                "y": "417.6",
+                "width": "81",
+                "height": "36",
+                "audio": "source/sound/orange.mp3"
+            },
+            {
+                "contentId": 8888,
+                "sortNum": "10",
+                "x": "529.6",
+                "y": "484.55",
+                "width": "81",
+                "height": "36",
+                "audio": "source/sound/yellow.mp3"
+            },
+			{
+			    "contentId": 8888,
+			    "sortNum": "99",
+			    "x": "145.2",
+			    "y": "36.8",
+			    "width": "148",
+			    "height": "38",
+			    "audio": "source/title/Getready.mp3"
+			},
+			{"contentId": 8888,
+                "sortNum": "1",
+                "x": "52.3",
+                "y": "98.95",
+                "width": "300",
+                "height": "36",
+                "audio": "source/title/A Listen Then point and say.mp3"
+            }
+        ],
+        "buttons": [
+            {
+                "id": "1",
+                "sourceType": "11",
+                "x": "352.75",
+                "y": "100.9",
+                "width": "33",
+                "height": "33",
+                "fileUrl": "source/sound/p42 Listen_Then point and say.mp3",
+                "breakpage": "",
+                "startpage": 0,
+                "endpage": 0,
+                "startindex": 0,
+                "endindex": 0,
+                "title": "课文录音"
+            },
+            {
+                "id": "2",
+                "sourceType": "93",
+                "x": "379.75",
+                "y": "100.9",
+                "width": "33",
+                "height": "33",
+                "fileUrl": "source/h5/u5_p42 A Listen_Then point and say/index.html",
+                "breakpage": "",
+                "startpage": 0,
+                "endpage": 0,
+                "startindex": 0,
+                "endindex": 0,
+                "title": "课件"
+            }
+        ]
+    },
+    "Message": ""
+};
