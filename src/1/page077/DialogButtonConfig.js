9978a5a70c9b7eb00b2ecc12d5ff5f65b56d0023
gzselian <3441775508@qq.com>
2025-06-11 15:02:46 +0800

    update src file from pakeplus

---
+++ src/1/page077/DialogButtonConfig.js
@@ -0,0 +1,241 @@
+
+var page077 = {
+    "Success": true,
+    "data": {
+        "pageContents": [
+			{
+			    "contentId": 8888,
+			    "sortNum": "0",
+			    "x": "258.5",
+			    "y": "110.9",
+			    "width": "234",
+			    "height": "30",
+			    "audio": "source/sound/p77 C Listen and chant 01.mp3"
+			},
+            {
+                "contentId": 8888,
+                "sortNum": "1",
+                "x": "59.4",
+                "y": "150.9",
+                "width": "315",
+                "height": "28",
+                "audio": "source/sound/p77 C Listen and chant 02.mp3"
+            },
+            {
+                "contentId": 8888,
+                "sortNum": "2",
+                "x": "59.15",
+                "y": "178.9",
+                "width": "303",
+                "height": "28",
+                "audio": "source/sound/p77 C Listen and chant 03.mp3"
+            },
+            {
+                "contentId": 8888,
+                "sortNum": "3",
+                "x": "58",
+                "y": "207.9",
+                "width": "355",
+                "height": "28",
+                "audio": "source/sound/p77 C Listen and chant 04.mp3"
+            },
+            {
+                "contentId": 8888,
+                "sortNum": "4",
+                "x": "59.4",
+                "y": "236.9",
+                "width": "300",
+                "height": "28",
+                "audio": "source/sound/p77 C Listen and chant 05.mp3"
+            },
+            {
+                "contentId": 8888,
+                "sortNum": "5",
+                "x": "58.65",
+                "y": "277.9",
+                "width": "340",
+                "height": "28",
+                "audio": "source/sound/p77 C Listen and chant 06.mp3"
+            },
+            {
+                "contentId": 8888,
+                "sortNum": "6",
+                "x": "59.4",
+                "y": "306.9",
+                "width": "315",
+                "height": "28",
+                "audio": "source/sound/p77 C Listen and chant 07.mp3"
+            },
+            {
+                "contentId": 8888,
+                "sortNum": "7",
+                "x": "57.95",
+                "y": "335.9",
+                "width": "363",
+                "height": "28",
+                "audio": "source/sound/p77 C Listen and chant 08.mp3"
+            },
+            {
+                "contentId": 8888,
+                "sortNum": "8",
+                "x": "58.25",
+                "y": "363.9",
+                "width": "259",
+                "height": "28",
+                "audio": "source/sound/p77 C Listen and chant 09.mp3"
+            },
+            {
+                "contentId": 8888,
+                "sortNum": "9",
+                "x": "100.1",
+                "y": "451.9",
+                "width": "336",
+                "height": "28",
+                "audio": "source/sound/p77 Review 01.mp3"
+            },
+            {
+                "contentId": 8888,
+                "sortNum": "10",
+                "x": "99",
+                "y": "481.9",
+                "width": "375.95",
+                "height": "28",
+                "audio": "source/sound/p77 Review 02.mp3"
+            },
+            {
+                "contentId": 8888,
+                "sortNum": "11",
+                "x": "474.85",
+                "y": "481.9",
+                "width": "211",
+                "height": "28",
+                "audio": "source/sound/p77 Review 03.mp3"
+            },
+            /* {
+                "contentId": 8888,
+                "sortNum": "12",
+                "x": "577.45",
+                "y": "481.9",
+                "width": "112",
+                "height": "28",
+                "audio": "source/sound/p77 Review 04.mp3"
+            }, */
+            {
+                "contentId": 8888,
+                "sortNum": "13",
+                "x": "95.6",
+                "y": "511.9",
+                "width": "345",
+                "height": "28",
+                "audio": "source/sound/p77 Review 05.mp3"
+            },
+            {
+                "contentId": 8888,
+                "sortNum": "14",
+                "x": "441.7",
+                "y": "511.9",
+                "width": "176",
+                "height": "28",
+                "audio": "source/sound/p77 Review 06.mp3"
+            },
+            {
+                "contentId": 8888,
+                "sortNum": "15",
+                "x": "95.5",
+                "y": "538.9",
+                "width": "335",
+                "height": "28",
+                "audio": "source/sound/p77 Review 07.mp3"
+            },
+            {
+                "contentId": 8888,
+                "sortNum": "16",
+                "x": "95.45",
+                "y": "566.9",
+                "width": "321",
+                "height": "28",
+                "audio": "source/sound/p77 Review 08.mp3"
+            },{
+                "contentId": 8888,
+                "sortNum": "1",
+                "x": "55.65",
+                "y": "70.95",
+                "width": "206",
+                "height": "36",
+                "audio": "source/title/C Listen and chant.mp3"
+            },
+			{
+                "contentId": 8888,
+                "sortNum": "1",
+                "x": "127.8",
+                "y": "412.85",
+                "width": "77",
+                "height": "32",
+                "audio": "source/title/p77 Review.mp3"
+            }
+        ],
+        "buttons": [
+            {
+                "id": "1",
+                "sourceType": "11",
+                "x": "259.75",
+                "y": "71.9",
+                "width": "33",
+                "height": "33",
+                "fileUrl": "source/sound/p77 C Listen and chant.mp3",
+                "breakpage": "",
+                "startpage": 0,
+                "endpage": 0,
+                "startindex": 0,
+                "endindex": 0,
+                "title": "课文录音"
+            },
+            {
+                "id": "2",
+                "sourceType": "93",
+                "x": "286.75",
+                "y": "71.9",
+                "width": "33",
+                "height": "33",
+                "fileUrl": "source/h5/u8_p77 C Listen and chant/index.html",
+                "breakpage": "",
+                "startpage": 0,
+                "endpage": 0,
+                "startindex": 0,
+                "endindex": 0,
+                "title": "动画"
+            },
+            {
+                "id": "3",
+                "sourceType": "93",
+                "x": "257.9",
+                "y": "613.9",
+                "width": "33",
+                "height": "33",
+                "fileUrl": "source/h5/u8_p77 D Ask and answer/index.html",
+                "breakpage": "",
+                "startpage": 0,
+                "endpage": 0,
+                "startindex": 0,
+                "endindex": 0,
+                "title": "课件"
+            },
+            {
+                "id": "4",
+                "sourceType": "93",
+                "x": "211.4",
+                "y": "412.9",
+                "width": "33",
+                "height": "33",
+                "fileUrl": "source/h5/u8_p77 Review/index.html",
+                "breakpage": "",
+                "startpage": 0,
+                "endpage": 0,
+                "startindex": 0,
+                "endindex": 0,
+                "title": "课件"
+            }
+        ]
+    },
+    "Message": ""
+};
