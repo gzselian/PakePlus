eb4c871c8ceef3007088d84fc326812a1304ad6b
gzselian <3441775508@qq.com>
2025-06-11 15:03:30 +0800

    update src file from pakeplus

---
+++ src/1/page080/page080.js
@@ -0,0 +1,346 @@
+(function (cjs, an) {
+
+var p; // shortcut to reference prototypes
+var lib={};var ss={};var img={};
+lib.ssMetadata = [
+		{name:"page080_atlas_1", frames: [[0,0,747,1046]]}
+];
+
+
+(lib.AnMovieClip = function(){
+	this.actionFrames = [];
+	this.ignorePause = false;
+	this.gotoAndPlay = function(positionOrLabel){
+		cjs.MovieClip.prototype.gotoAndPlay.call(this,positionOrLabel);
+	}
+	this.play = function(){
+		cjs.MovieClip.prototype.play.call(this);
+	}
+	this.gotoAndStop = function(positionOrLabel){
+		cjs.MovieClip.prototype.gotoAndStop.call(this,positionOrLabel);
+	}
+	this.stop = function(){
+		cjs.MovieClip.prototype.stop.call(this);
+	}
+}).prototype = p = new cjs.MovieClip();
+// symbols:
+
+
+
+(lib.bg_red = function() {
+	this.initialize(ss["page080_atlas_1"]);
+	this.gotoAndStop(0);
+}).prototype = p = new cjs.Sprite();
+
+
+
+(lib.元件1 = function(mode,startPosition,loop,reversed) {
+if (loop == null) { loop = true; }
+if (reversed == null) { reversed = false; }
+	var props = new Object();
+	props.mode = mode;
+	props.startPosition = startPosition;
+	props.labels = {};
+	props.loop = loop;
+	props.reversed = reversed;
+	cjs.MovieClip.apply(this,[props]);
+
+	// 图层_1
+	this.shape = new cjs.Shape();
+	this.shape.graphics.f("#FF0000").s().p("EgihAEOIAAobMBFDAAAIAAIbg");
+	this.shape.setTransform(221,27);
+	this.shape._off = true;
+
+	this.timeline.addTween(cjs.Tween.get(this.shape).wait(3).to({_off:false},0).wait(1));
+
+	this._renderFirstFrame();
+
+}).prototype = p = new cjs.MovieClip();
+p.nominalBounds = new cjs.Rectangle(0,0,442,54);
+
+
+// stage content:
+(lib.page080 = function(mode,startPosition,loop,reversed) {
+if (loop == null) { loop = true; }
+if (reversed == null) { reversed = false; }
+	var props = new Object();
+	props.mode = mode;
+	props.startPosition = startPosition;
+	props.labels = {};
+	props.loop = loop;
+	props.reversed = reversed;
+	cjs.MovieClip.apply(this,[props]);
+
+	// 图层_1
+	this.cont_23 = new lib.元件1();
+	this.cont_23.name = "cont_23";
+	this.cont_23.setTransform(813.75,791.6,0.4299,0.5185,0,0,0,1716.2,25.2);
+	new cjs.ButtonHelper(this.cont_23, 0, 1, 2, false, new lib.元件1(), 3);
+
+	this.cont_22 = new lib.元件1();
+	this.cont_22.name = "cont_22";
+	this.cont_22.setTransform(1543.4,764.6,0.8552,0.5185,0,0,0,1713.4,25.2);
+	new cjs.ButtonHelper(this.cont_22, 0, 1, 2, false, new lib.元件1(), 3);
+
+	this.cont_21 = new lib.元件1();
+	this.cont_21.name = "cont_21";
+	this.cont_21.setTransform(1311.2,736.6,0.6787,0.5185,0,0,0,1712.3,25.2);
+	new cjs.ButtonHelper(this.cont_21, 0, 1, 2, false, new lib.元件1(), 3);
+
+	this.cont_20 = new lib.元件1();
+	this.cont_20.name = "cont_20";
+	this.cont_20.setTransform(359.1,736.6,0.1652,0.5185,0,0,0,1719.2,25.2);
+	new cjs.ButtonHelper(this.cont_20, 0, 1, 2, false, new lib.元件1(), 3);
+
+	this.cont_19 = new lib.元件1();
+	this.cont_19.name = "cont_19";
+	this.cont_19.setTransform(1444.85,708.6,0.7984,0.5185,0,0,0,1713.4,25.2);
+	new cjs.ButtonHelper(this.cont_19, 0, 1, 2, false, new lib.元件1(), 3);
+
+	this.cont_18 = new lib.元件1();
+	this.cont_18.name = "cont_18";
+	this.cont_18.setTransform(1059.75,679.6,0.4909,0.5185,0,0,0,1712.5,25.2);
+	new cjs.ButtonHelper(this.cont_18, 0, 1, 2, false, new lib.元件1(), 3);
+
+	this.cont_17 = new lib.元件1();
+	this.cont_17.name = "cont_17";
+	this.cont_17.setTransform(622.5,678.6,0.319,0.5185,0,0,0,1713.7,25.2);
+	new cjs.ButtonHelper(this.cont_17, 0, 1, 2, false, new lib.元件1(), 3);
+
+	this.cont_16 = new lib.元件1();
+	this.cont_16.name = "cont_16";
+	this.cont_16.setTransform(1177.55,649.6,0.5769,0.5185,0,0,0,1711.2,25.2);
+	new cjs.ButtonHelper(this.cont_16, 0, 1, 2, false, new lib.元件1(), 3);
+
+	this.cont_15 = new lib.元件1();
+	this.cont_15.name = "cont_15";
+	this.cont_15.setTransform(518.35,649.6,0.2579,0.5185,0,0,0,1712.8,25.2);
+	new cjs.ButtonHelper(this.cont_15, 0, 1, 2, false, new lib.元件1(), 3);
+
+	this.cont_14 = new lib.元件1();
+	this.cont_14.name = "cont_14";
+	this.cont_14.setTransform(1508.25,619.6,0.837,0.5185,0,0,0,1707.6,25.2);
+	new cjs.ButtonHelper(this.cont_14, 0, 1, 2, false, new lib.元件1(), 3);
+
+	this.cont_13 = new lib.元件1();
+	this.cont_13.name = "cont_13";
+	this.cont_13.setTransform(958.1,507.65,0.3801,0.5185,0,0,0,1710.8,25.2);
+	new cjs.ButtonHelper(this.cont_13, 0, 1, 2, false, new lib.元件1(), 3);
+
+	this.cont_12 = new lib.元件1();
+	this.cont_12.name = "cont_12";
+	this.cont_12.setTransform(946.65,478.65,0.2217,0.5185,0,0,0,1716.5,25.2);
+	new cjs.ButtonHelper(this.cont_12, 0, 1, 2, false, new lib.元件1(), 3);
+
+	this.cont_11 = new lib.元件1();
+	this.cont_11.name = "cont_11";
+	this.cont_11.setTransform(1298.6,478.65,0.5792,0.5185,0,0,0,1708.2,25.2);
+	new cjs.ButtonHelper(this.cont_11, 0, 1, 2, false, new lib.元件1(), 3);
+
+	this.cont_10 = new lib.元件1();
+	this.cont_10.name = "cont_10";
+	this.cont_10.setTransform(1642.35,448.65,0.7805,0.5185,0,0,0,1707.5,25.2);
+	new cjs.ButtonHelper(this.cont_10, 0, 1, 2, false, new lib.元件1(), 3);
+
+	this.cont_9 = new lib.元件1();
+	this.cont_9.name = "cont_9";
+	this.cont_9.setTransform(1046.5,419.65,0.3054,0.5185,0,0,0,1710.8,25.2);
+	new cjs.ButtonHelper(this.cont_9, 0, 1, 2, false, new lib.元件1(), 3);
+
+	this.cont_8 = new lib.元件1();
+	this.cont_8.name = "cont_8";
+	this.cont_8.setTransform(1125.95,419.65,0.4773,0.5185,0,0,0,1706.7,25.2);
+	new cjs.ButtonHelper(this.cont_8, 0, 1, 2, false, new lib.元件1(), 3);
+
+	this.cont_7 = new lib.元件1();
+	this.cont_7.name = "cont_7";
+	this.cont_7.setTransform(647.55,330.7,0.3439,0.5185,0,0,0,1712.2,25.2);
+	new cjs.ButtonHelper(this.cont_7, 0, 1, 2, false, new lib.元件1(), 3);
+
+	this.cont_6 = new lib.元件1();
+	this.cont_6.name = "cont_6";
+	this.cont_6.setTransform(1454.85,300.7,0.8167,0.5185,0,0,0,1708.2,25.2);
+	new cjs.ButtonHelper(this.cont_6, 0, 1, 2, false, new lib.元件1(), 3);
+
+	this.cont_5 = new lib.元件1();
+	this.cont_5.name = "cont_5";
+	this.cont_5.setTransform(528,270.7,0.0746,0.5185,0,0,0,1726.2,25.2);
+	new cjs.ButtonHelper(this.cont_5, 0, 1, 2, false, new lib.元件1(), 3);
+
+	this.cont_4 = new lib.元件1();
+	this.cont_4.name = "cont_4";
+	this.cont_4.setTransform(968,270.7,0.4478,0.5185,0,0,0,1712.7,25.2);
+	new cjs.ButtonHelper(this.cont_4, 0, 1, 2, false, new lib.元件1(), 3);
+
+	this.cont_3 = new lib.元件1();
+	this.cont_3.name = "cont_3";
+	this.cont_3.setTransform(597.95,270.7,0.3145,0.5185,0,0,0,1711,25.2);
+	new cjs.ButtonHelper(this.cont_3, 0, 1, 2, false, new lib.元件1(), 3);
+
+	this.cont_2 = new lib.元件1();
+	this.cont_2.name = "cont_2";
+	this.cont_2.setTransform(872.55,240.7,0.3393,0.5185,0,0,0,1709.2,25.2);
+	new cjs.ButtonHelper(this.cont_2, 0, 1, 2, false, new lib.元件1(), 3);
+
+	this.cont_1 = new lib.元件1();
+	this.cont_1.name = "cont_1";
+	this.cont_1.setTransform(948.6,240.7,0.5203,0.5185,0,0,0,1706,25.2);
+	new cjs.ButtonHelper(this.cont_1, 0, 1, 2, false, new lib.元件1(), 3);
+
+	this.btn_92 = new lib.元件1();
+	this.btn_92.name = "btn_92";
+	this.btn_92.setTransform(444.05,54.75,0.0747,0.6111,0,0,0,1699,25.9);
+	new cjs.ButtonHelper(this.btn_92, 0, 1, 2, false, new lib.元件1(), 3);
+
+	this.cont_0 = new lib.元件1();
+	this.cont_0.name = "cont_0";
+	this.cont_0.setTransform(2466.4,139.25,1.3982,1.1111,0,0,0,1703,25.4);
+	new cjs.ButtonHelper(this.cont_0, 0, 1, 2, false, new lib.元件1(), 3);
+
+	this.btn_11 = new lib.元件1();
+	this.btn_11.name = "btn_11";
+	this.btn_11.setTransform(410.1,54.75,0.0747,0.6111,0,0,0,1699,25.9);
+	new cjs.ButtonHelper(this.btn_11, 0, 1, 2, false, new lib.元件1(), 3);
+
+	this.timeline.addTween(cjs.Tween.get({}).to({state:[{t:this.btn_11},{t:this.cont_0},{t:this.btn_92},{t:this.cont_1},{t:this.cont_2},{t:this.cont_3},{t:this.cont_4},{t:this.cont_5},{t:this.cont_6},{t:this.cont_7},{t:this.cont_8},{t:this.cont_9},{t:this.cont_10},{t:this.cont_11},{t:this.cont_12},{t:this.cont_13},{t:this.cont_14},{t:this.cont_15},{t:this.cont_16},{t:this.cont_17},{t:this.cont_18},{t:this.cont_19},{t:this.cont_20},{t:this.cont_21},{t:this.cont_22},{t:this.cont_23}]}).wait(1));
+
+	// 图层_2
+	this.instance = new lib.bg_red();
+	this.instance.setTransform(1,0);
+
+	this.timeline.addTween(cjs.Tween.get(this.instance).wait(1));
+
+	this._renderFirstFrame();
+
+}).prototype = p = new lib.AnMovieClip();
+p.nominalBounds = new cjs.Rectangle(374.5,523,373.5,523);
+// library properties:
+lib.properties = {
+	id: '35BF5C55F63D7D43B29AB118E3A283C7',
+	width: 747,
+	height: 1046,
+	fps: 30,
+	color: "#FFFFFF",
+	opacity: 1.00,
+	manifest: [
+		{src:"images/page080_atlas_1.png", id:"page080_atlas_1"}
+	],
+	preloads: []
+};
+
+
+
+// bootstrap callback support:
+
+(lib.Stage = function(canvas) {
+	createjs.Stage.call(this, canvas);
+}).prototype = p = new createjs.Stage();
+
+p.setAutoPlay = function(autoPlay) {
+	this.tickEnabled = autoPlay;
+}
+p.play = function() { this.tickEnabled = true; this.getChildAt(0).gotoAndPlay(this.getTimelinePosition()) }
+p.stop = function(ms) { if(ms) this.seek(ms); this.tickEnabled = false; }
+p.seek = function(ms) { this.tickEnabled = true; this.getChildAt(0).gotoAndStop(lib.properties.fps * ms / 1000); }
+p.getDuration = function() { return this.getChildAt(0).totalFrames / lib.properties.fps * 1000; }
+
+p.getTimelinePosition = function() { return this.getChildAt(0).currentFrame / lib.properties.fps * 1000; }
+
+an.bootcompsLoaded = an.bootcompsLoaded || [];
+if(!an.bootstrapListeners) {
+	an.bootstrapListeners=[];
+}
+
+an.bootstrapCallback=function(fnCallback) {
+	an.bootstrapListeners.push(fnCallback);
+	if(an.bootcompsLoaded.length > 0) {
+		for(var i=0; i<an.bootcompsLoaded.length; ++i) {
+			fnCallback(an.bootcompsLoaded[i]);
+		}
+	}
+};
+
+an.compositions = an.compositions || {};
+an.compositions['35BF5C55F63D7D43B29AB118E3A283C7'] = {
+	getStage: function() { return exportRoot.stage; },
+	getLibrary: function() { return lib; },
+	getSpriteSheet: function() { return ss; },
+	getImages: function() { return img; }
+};
+
+an.compositionLoaded = function(id) {
+	an.bootcompsLoaded.push(id);
+	for(var j=0; j<an.bootstrapListeners.length; j++) {
+		an.bootstrapListeners[j](id);
+	}
+}
+
+an.getComposition = function(id) {
+	return an.compositions[id];
+}
+
+
+an.makeResponsive = function(isResp, respDim, isScale, scaleType, domContainers) {		
+	var lastW, lastH, lastS=1;		
+	window.addEventListener('resize', resizeCanvas);		
+	resizeCanvas();		
+	function resizeCanvas() {			
+		var w = lib.properties.width, h = lib.properties.height;			
+		var iw = window.innerWidth, ih=window.innerHeight;			
+		var pRatio = window.devicePixelRatio || 1, xRatio=iw/w, yRatio=ih/h, sRatio=1;			
+		if(isResp) {                
+			if((respDim=='width'&&lastW==iw) || (respDim=='height'&&lastH==ih)) {                    
+				sRatio = lastS;                
+			}				
+			else if(!isScale) {					
+				if(iw<w || ih<h)						
+					sRatio = Math.min(xRatio, yRatio);				
+			}				
+			else if(scaleType==1) {					
+				sRatio = Math.min(xRatio, yRatio);				
+			}				
+			else if(scaleType==2) {					
+				sRatio = Math.max(xRatio, yRatio);				
+			}			
+		}
+		domContainers[0].width = w * pRatio * sRatio;			
+		domContainers[0].height = h * pRatio * sRatio;
+		domContainers.forEach(function(container) {				
+			container.style.width = w * sRatio + 'px';				
+			container.style.height = h * sRatio + 'px';			
+		});
+		stage.scaleX = pRatio*sRatio;			
+		stage.scaleY = pRatio*sRatio;
+		lastW = iw; lastH = ih; lastS = sRatio;            
+		stage.tickOnUpdate = false;            
+		stage.update();            
+		stage.tickOnUpdate = true;		
+	}
+}
+an.handleSoundStreamOnTick = function(event) {
+	if(!event.paused){
+		var stageChild = stage.getChildAt(0);
+		if(!stageChild.paused || stageChild.ignorePause){
+			stageChild.syncStreamSounds();
+		}
+	}
+}
+an.handleFilterCache = function(event) {
+	if(!event.paused){
+		var target = event.target;
+		if(target){
+			if(target.filterCacheList){
+				for(var index = 0; index < target.filterCacheList.length ; index++){
+					var cacheInst = target.filterCacheList[index];
+					if((cacheInst.startFrame <= target.currentFrame) && (target.currentFrame <= cacheInst.endFrame)){
+						cacheInst.instance.cache(cacheInst.x, cacheInst.y, cacheInst.w, cacheInst.h);
+					}
+				}
+			}
+		}
+	}
+}
+
+
+})(createjs = createjs||{}, AdobeAn = AdobeAn||{});
+var createjs, AdobeAn;
