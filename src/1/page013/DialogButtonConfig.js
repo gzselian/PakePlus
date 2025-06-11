9f00cf565b3cb066f1250da65360cc66f2aea991
gzselian <3441775508@qq.com>
2025-06-11 14:50:01 +0800

    update src file from pakeplus

---
+++ src/1/page013/DialogButtonConfig.js
@@ -0,0 +1,25 @@
+var page013 = {
+	"Success": true,
+	"data": {
+		"pageContents": [
+			
+		],
+		"buttons": [
+			{
+				"id": "1",
+				"sourceType": "94",
+				"x": "218.05",
+				"y": "203.05",
+				"width": "33",
+				"height": "33",
+				"fileUrl": "source/h5/u2_P13 C Look and tick/index.html",
+				"startpage": 0,
+				"endpage": 0,
+				"startindex": 0,
+				"endindex": 0,
+				"title": "课件"
+			}
+		],
+	},
+	"Message": ""
+};
