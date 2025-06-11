7434a58522840c612d8c54f957a9751ea487f451
gzselian <3441775508@qq.com>
2025-06-11 14:59:06 +0800

    update src file from pakeplus

---
+++ src/1/page057/DialogButtonConfig.js
@@ -0,0 +1,205 @@
+
+var page057 = {
+    "Success": true,
+    "data": {
+        "pageContents": [
+            
+            {
+                "contentId": 8888,
+                "sortNum": "1",
+                "x": "262.9",
+                "y": "110.9",
+                "width": "223.95",
+                "height": "30",
+                "audio": "source/sound/P57 C Listen and chant 01.mp3"
+            },
+            {
+                "contentId": 8888,
+                "sortNum": "2",
+                "x": "113.2",
+                "y": "174.9",
+                "width": "250",
+                "height": "30",
+                "audio": "source/sound/P57 C Listen and chant 03.mp3"
+            },
+            {
+                "contentId": 8888,
+                "sortNum": "0",
+                "x": "112.65",
+                "y": "145.9",
+                "width": "206",
+                "height": "30",
+                "audio": "source/sound/P57 C Listen and chant 02.mp3"
+            },{
+                "contentId": 8888,
+                "sortNum": "3",
+                "x": "112.3",
+                "y": "204.9",
+                "width": "181",
+                "height": "30",
+                "audio": "source/sound/P57 C Listen and chant 04.mp3"
+            },
+            {
+                "contentId": 8888,
+                "sortNum": "4",
+                "x": "113.15",
+                "y": "234.9",
+                "width": "192",
+                "height": "30",
+                "audio": "source/sound/P57 C Listen and chant 05.mp3"
+            },
+            {
+                "contentId": 8888,
+                "sortNum": "5",
+                "x": "113.25",
+                "y": "266.9",
+                "width": "201",
+                "height": "30",
+                "audio": "source/sound/P57 C Listen and chant 06.mp3"
+            },
+            {
+                "contentId": 8888,
+                "sortNum": "6",
+                "x": "112.75",
+                "y": "297.9",
+                "width": "240",
+                "height": "30",
+                "audio": "source/sound/P57 C Listen and chant 07.mp3"
+            },
+            {
+                "contentId": 8888,
+                "sortNum": "7",
+                "x": "113.35",
+                "y": "325.9",
+                "width": "236.95",
+                "height": "30",
+                "audio": "source/sound/P57 C Listen and chant 08.mp3"
+            },
+            {
+                "contentId": 8888,
+                "sortNum": "8",
+                "x": "111.95",
+                "y": "356.9",
+                "width": "237",
+                "height": "30",
+                "audio": "source/sound/P57 C Listen and chant 09.mp3"
+            },
+            {
+                "contentId": 8888,
+                "sortNum": "9",
+                "x": "96.1",
+                "y": "452.9",
+                "width": "249",
+                "height": "30",
+                "audio": "source/sound/p57 Learn 01.mp3"
+            },
+            {
+                "contentId": 8888,
+                "sortNum": "10",
+                "x": "506.9",
+                "y": "442.9",
+                "width": "174",
+                "height": "30",
+                "audio": "source/sound/p57 Learn 04.mp3"
+            },
+            {
+                "contentId": 8888,
+                "sortNum": "11",
+                "x": "95.7",
+                "y": "482.9",
+                "width": "304",
+                "height": "30",
+                "audio": "source/sound/p57 Learn 02.mp3"
+            },
+            {
+                "contentId": 8888,
+                "sortNum": "12",
+                "x": "94.9",
+                "y": "512.9",
+                "width": "366",
+                "height": "30",
+                "audio": "source/sound/p57 Learn 03.mp3"
+            },{
+                "contentId": 8888,
+                "sortNum": "1",
+                "x": "55.65",
+                "y": "70.95",
+                "width": "206",
+                "height": "36",
+                "audio": "source/title/C Listen and chant.mp3"
+            },
+			{
+                "contentId": 8888,
+                "sortNum": "1",
+                "x": "128.05",
+                "y": "413.85",
+                "width": "66",
+                "height": "32",
+                "audio": "source/title/Learn.mp3"
+            }
+        ],
+        "buttons": [
+            {
+                "id": "1",
+                "sourceType": "11",
+                "x": "259.75",
+                "y": "71.9",
+                "width": "33",
+                "height": "33",
+                "fileUrl": "source/sound/P57 C Listen and chant.mp3",
+                "breakpage": "",
+                "startpage": 0,
+                "endpage": 0,
+                "startindex": 0,
+                "endindex": 0,
+                "title": "课文录音"
+            },
+            {
+                "id": "2",
+                "sourceType": "93",
+                "x": "291.75",
+                "y": "71.9",
+                "width": "33",
+                "height": "33",
+                "fileUrl": "source/h5/u6_p57 C Listen and chant/index.html",
+                "breakpage": "",
+                "startpage": 0,
+                "endpage": 0,
+                "startindex": 0,
+                "endindex": 0,
+                "title": "课件"
+            },
+            {
+                "id": "3",
+                "sourceType": "93",
+                "x": "226.75",
+                "y": "583.85",
+                "width": "33",
+                "height": "33",
+                "fileUrl": "source/h5/u6_p57 D Play a game/index.html",
+                "breakpage": "",
+                "startpage": 0,
+                "endpage": 0,
+                "startindex": 0,
+                "endindex": 0,
+                "title": "课件"
+            },
+            {
+                "id": "4",
+                "sourceType": "93",
+                "x": "202.4",
+                "y": "414.85",
+                "width": "33",
+                "height": "33",
+                "fileUrl": "source/h5/u6_p57 Learn/index.html",
+                "breakpage": "",
+                "startpage": 0,
+                "endpage": 0,
+                "startindex": 0,
+                "endindex": 0,
+                "title": "课件"
+            }
+        ]
+    },
+    "Message": ""
+};
