f8e923312f59d58ba6232ea160d5be0a209c6b82
gzselian <3441775508@qq.com>
2025-06-11 18:19:47 +0800

    update src file from pakeplus

---
+++ src/1/source/h5/u6_p54 Explore1/js/subtitleDB.js
@@ -0,0 +1,81 @@
+var movPath="../../mov/P54 Explore1.mp4";
+var soundPath="source/01bg.mp3";
+var movRatio="16:9";
+
+var subtitleJson = {
+
+"wordChArr": [
+    "致：Lotte",
+    "来自：Panpan",
+    "主题：我的新家",
+	
+    "亲爱的Lotte，",
+    "我迫不及待地想要给你展示我的新家！",
+    "家里有一个厨房、一个餐厅、",
+    "一个客厅、一个浴室、",
+    "三间卧室和一个花园。",
+    "在我的卧室里，有一张书桌。",
+    "我喜欢在书桌上阅读和写作。",
+    "我的卧室前面有一个花园。",
+    "我喜欢看向窗外的风景。",
+    "你的家呢？",
+    "你的卧室里有什么？",
+    "最美好的祝愿，",
+    "Panpan"
+],
+"wordArr": [
+	"To: Lotte",
+    "From: Panpan",
+    "Subject: My new home",
+	
+    "Dear Lotte,",
+    "I can’t wait to show you my new home!",
+    "There’s a kitchen, a dining room,",
+    "a living room, a bathroom, ",
+    "three bedrooms and a garden.",
+    "In my bedroom, there’s a desk.",
+    "I like reading and writing at my desk.",
+    "There’s a garden in front of my bedroom.",
+    "I like looking out of the window.",
+    "How about your home?",
+    "What’s in your bedroom?",
+    "Best wishes, ",
+    "Panpan"
+],
+"timeArr": [
+"5000", "7041",
+"7416", "9125",
+"9666", "12875",
+"13375", "15041",
+"15458", "19916",
+"20333", "24250",
+"24458", "27791",
+"28041", "31625",
+"31875", "36000",
+"36125", "41333",
+"41833", "46750",
+"47083", "51166",
+"51875", "54500",
+"54875", "57208",
+"57958", "58958",
+"59083", "60250"
+], // 31
+"rolepoints": [
+    "a", "a", "a", 
+	"p", "p", "p","p", "p", "p", "p", "p", "p", "p", "p", "p", "p"
+],
+
+
+
+// 齐声
+// 1:05.349-1:07.919
+// 角色出现顺序
+
+"allrole":
+     ["a","p"]
+};
+var rolelist= ["a","p"];
+
+var Allrolepoints = [];
+
+ 
