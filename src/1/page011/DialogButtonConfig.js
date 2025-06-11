6ab302c201b58c2e2e319da9fbad8d6a775322fb
gzselian <3441775508@qq.com>
2025-06-11 14:49:25 +0800

    update src file from pakeplus

---
+++ src/1/page011/DialogButtonConfig.js
@@ -0,0 +1,31 @@
+var page011 = {
+	"Success": true,
+	"data": {
+		"pageContents": [{
+			"contentId": 8888,
+			"sortNum": "1",
+			"x": "99.65",
+			"y": "115.3",
+			"width": "436",
+			"height": "46",
+			"audio": "source/sound/p11 How do we make friends.mp3"
+		}, ],
+		"buttons": [
+		{
+				"id": "0",
+				"sourceType": "92",
+				"x": "680",
+				"y": "920",
+				"width": "60",
+				"height": "60",
+				"fileUrl": "source/mov/daor/u2_daor.mp4",
+				// "breakpage":"33,6",
+				"startpage": 0,
+				"endpage": 0,
+				"startindex": 0,
+				"endindex": 0,
+				"title": "导入视频"
+			}],
+	},
+	"Message": ""
+};
