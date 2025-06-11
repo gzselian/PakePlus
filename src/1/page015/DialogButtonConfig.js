06f3a1000b2d188c1c217e42ec9da8eff5fd8aeb
gzselian <3441775508@qq.com>
2025-06-11 14:50:32 +0800

    update src file from pakeplus

---
+++ src/1/page015/DialogButtonConfig.js
@@ -0,0 +1,63 @@
+var page015 = {
+	"Success": true,
+	"data": {
+		"pageContents": [
+		    {
+                "contentId": 8888,
+                "sortNum": "13",
+                "x": "410.35",
+                "y": "102.8",
+                "width": "138.7",
+                "height": "79.3",
+                "audio": "source/sound/p14 Explore 13.mp3"
+            },
+            {
+                "contentId": 8888,
+                "sortNum": "14",
+                "x": "179.35",
+                "y": "226.85",
+                "width": "94.7",
+                "height": "26.3",
+                "audio": "source/sound/p14 Explore 14.mp3"
+            },
+            {
+                "contentId": 8888,
+                "sortNum": "15",
+                "x": "570.35",
+                "y": "241.85",
+                "width": "104.7",
+                "height": "52.3",
+                "audio": "source/sound/p14 Explore 15.mp3"
+            },
+            {
+                "contentId": 8888,
+                "sortNum": "16",
+                "x": "329.35",
+                "y": "592.85",
+                "width": "179.7",
+                "height": "49.35",
+                "audio": "source/sound/p14 Explore 16.mp3"
+            },
+            {
+                "contentId": 8888,
+                "sortNum": "17",
+                "x": "329.65",
+                "y": "645.2",
+                "width": "139.65",
+                "height": "26.3",
+                "audio": "source/sound/p14 Explore 17.mp3"
+            },
+            {
+                "contentId": 8888,
+                "sortNum": "18",
+                "x": "600.35",
+                "y": "615.8",
+                "width": "79.7",
+                "height": "26.3",
+                "audio": "source/sound/p14 Explore 18.mp3"
+            }
+		],
+		"buttons": [],
+	},
+	"Message": ""
+};
