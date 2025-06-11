c75276eb831daf3d7a88c64d9deee4c78e4507a4
gzselian <3441775508@qq.com>
2025-06-11 15:29:10 +0800

    update src file from pakeplus

---
+++ src/1/source/h5/u2_p14 Explore/js/subtitleDB.js
@@ -0,0 +1,107 @@
+var movPath="../../mov/P14 Explore.mp4";
+var soundPath="source/01bg.mp3";
+var movRatio="16:9";
+
+var subtitleJson = {
+"wordChArr": [
+    "你好！我的名字叫丹丹。",
+	"你的沙堡太棒了！",
+	
+	"谢谢你！我是兰兰。",
+	"那是我的弟弟，军军。",
+	"你喜欢玩沙子吗？",
+	
+	"是的，我喜欢。",
+	
+	"太棒了！你可以加入我！",
+	
+	"嘿，大家好。我是凯凯。",
+	"我是丹丹的哥哥。",
+	
+	"过来，凯凯！",
+	"来见见我的新朋友兰兰",
+	"和她的弟弟军军。",
+	
+	"你好，凯凯。",
+	
+	"你喜欢打沙滩排球吗？",
+	
+	"是的，我喜欢！",
+	
+	"让我们一起玩吧！",
+	
+	"兰兰和军军有了新朋友。",
+	"我太高兴了！",
+	
+	"我也很高兴。",
+],
+"wordArr": [
+    "Hi there! My name is Dandan. ",//d
+	"Your sandcastle is great!",//d
+	
+	"Thank you. I'm Lanlan.",//l
+	"That’s my brother, Junjun. ",
+	"Do you like playing in the sand?",
+	
+	"Yes, I do.",//d
+	
+	"Great! You can join me!",//l
+	
+	"Hey there. I'm Kaikai.",//k
+	"I’m Dandan’s brother.",
+	
+	"Come here, Kaikai! ",//d
+	"Meet my new friend Lanlan",
+	"and her brother Junjun.",
+	
+	"Hello, Kaikai.",//l&j
+	
+	"Do you like playing beach volleyball?",//k
+	
+	"Yes, I do!",//j
+	
+	"Let’s play together!",//k
+	
+	"Lanlan and Junjun have new friends.",//lm
+	"I’m so happy!",
+	
+	"Me too.",//l
+	],
+
+"timeArr": [
+"6625", "10791",
+"11083", "15291",
+"15500", "19041",
+"19166", "22083",
+"24041", "27125",
+"27291", "30041",
+"30250", "33625",
+"35708", "39333",
+"39375", "42416",
+"42625", "45375",
+"45416", "49000",
+"49041", "51666",
+"51958", "54291",
+"55583", "59875",
+"60000", "62708",
+"63041", "65958",
+"75750", "80166",
+"80208", "82708",
+"83125", "85083"
+] // 19
+
+
+,
+// 齐声
+// 1:05.349-1:07.919
+// 角色出现顺序
+"rolepoints":
+     ["d","d","l","l","l","d","l","k","k","d","d","d","lj","k","j","k","lm","lm","ld"],
+"allrole":
+     ["d","l","k","lj","j","lm","ld"],
+};
+var rolelist=["Teacher","Wen Jie","Jia Li","Girl 1","Boy 1","Classmates"];
+
+var Allrolepoints = [];
+
+ 
