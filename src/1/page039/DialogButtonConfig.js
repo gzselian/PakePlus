414fa4786321f285173b1d4e598a06430c83bdf0
gzselian <3441775508@qq.com>
2025-06-11 14:55:31 +0800

    update src file from pakeplus

---
+++ src/1/page039/DialogButtonConfig.js
@@ -0,0 +1,25 @@
+var page039 = {
+    "Success": true,
+    "data": {
+        "pageContents": [
+        ],
+        "buttons": [
+            {
+                "id": "1",
+                "sourceType": "93",
+                "x": "236.55",
+                "y": "67.5",
+                "width": "33",
+                "height": "33",
+                 "fileUrl": "source/h5/u4_p39 B Look and talk/index.html",
+                "breakpage": "",
+                "startpage": 0,
+                "endpage": 0,
+                "startindex": 0,
+                "endindex": 0,
+                "title": "练习"
+            }
+        ]
+    },
+    "Message": ""
+};
