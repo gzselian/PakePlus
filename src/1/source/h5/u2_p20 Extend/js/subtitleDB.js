af0c1619cac1039152b1c9dc3d053a6ef5ba0129
gzselian <3441775508@qq.com>
2025-06-11 15:41:04 +0800

    update src file from pakeplus

---
+++ src/1/source/h5/u2_p20 Extend/js/subtitleDB.js
@@ -0,0 +1,95 @@
+var movPath="../../mov/P20 Extend.mp4";
+var soundPath="source/01bg.mp3";
+var movRatio="16:9";
+
+var subtitleJson = {
+"wordChArr": [
+    "Elliot很伤心。",
+	"他看见一只乌龟。",
+	"你好，我是Tuti。",
+	"我是Elliot。",
+	"我找不到我的家人。",
+	"让我们找一找。",
+	"看！那是你的家人吗？",
+	"不！那些是斑马。",
+	"Elliot和Tuti一起吃，玩耍和睡觉。",
+	"看！那是你的家人吗？",
+	"不！那些是狮子。",
+	"Elliot和Tuti感到很害怕。",
+	"他们逃跑了。",
+	"看！大象。",
+	"那是我的家人。",
+	"是的！",
+	"谢谢你，Tuti。",
+	"Elliot交到了一个新朋友。"
+],
+"wordArr": [
+    "Elliot is sad.",//p
+	"He sees a tortoise.",
+	
+	"Hello, I’m Tuti.",//t
+	
+	"I'm Elliot.",//e
+	"I can’t find my family.",
+	
+	"Let's look.",//t
+	"Look! Is that your family?",
+	
+	"No! Those are zebras.",//e
+	
+	"Elliot and Tuti eat, play and sleep.",//p
+	
+	"Look! Is that your family?",//t
+	
+	"No! Those are lions.",//e
+	
+	"Elliot and Tuti are afraid. ",//p
+	"They run away.",
+	
+	"Look! Elephants.",//t
+	
+	"That’s my family.",//e
+	
+	"YES!",//t
+	
+	"Thanks, Tuti.",//e
+	"Elliot gets a new friend.",//p
+	],
+
+"timeArr": [
+"4066", "7266",
+"7300", "10133",
+"12033", "15500",
+"15700", "18033",
+"18066", "21333",
+
+"21466", "23833",
+"31500", "36200",
+"36400", "41800",
+"42366", "47966",
+"56100", "61266",
+
+"64833", "69900",
+"70100", "74600",
+"74633", "77033",
+"83600", "87666",
+"90700", "94233",
+
+"94466", "96500",
+"97333", "99866",
+"100233", "104366"
+] ,// 18
+
+// 齐声
+// 1:05.349-1:07.919
+// 角色出现顺序
+"rolepoints":
+     ["p","p","t","e","e","t","t","e","p","t","e","p","p","t","e","t","e","p"],
+"allrole":
+     ["p","t","e"]
+};
+var rolelist=["Teacher","Wen Jie","Jia Li","Girl 1","Boy 1","Classmates"];
+
+var Allrolepoints = [];
+
+ 
