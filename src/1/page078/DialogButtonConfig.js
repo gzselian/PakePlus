f03d55a9fea2a1550e0b43b48411da6d928d35f6
gzselian <3441775508@qq.com>
2025-06-11 15:02:57 +0800

    update src file from pakeplus

---
+++ src/1/page078/DialogButtonConfig.js
@@ -0,0 +1,74 @@
+
+var page078 = {
+    "Success": true,
+    "data": {
+        "pageContents": [
+			{
+                "contentId": 8888,
+                "sortNum": "97",
+                "x": "151.55",
+                "y": "37",
+                "width": "196",
+                "height": "38",
+                "audio": "source/title/Communicate.mp3"
+            },
+			{
+                "contentId": 8888,
+                "sortNum": "1",
+                "x": "59.95",
+                "y": "111.9",
+                "width": "304",
+                "height": "32",
+                "audio": "source/title/Listen and match then act.mp3"
+            }
+        ],
+        "buttons": [
+            {
+                "id": "1",
+                "sourceType": "11",
+                "x": "364.75",
+                "y": "109.9",
+                "width": "33",
+                "height": "33",
+                "fileUrl": "source/sound/p78 A Listen and match_Then act.mp3",
+                "breakpage": "",
+                "startpage": 0,
+                "endpage": 0,
+                "startindex": 0,
+                "endindex": 0,
+                "title": "课文录音"
+            },
+            {
+                "id": "2",
+                "sourceType": "93",
+                "x": "423.75",
+                "y": "109.9",
+                "width": "33",
+                "height": "33",
+                "fileUrl": "source/h5/u8_p78 A Listen and match_Then act02/index.html",
+                "breakpage": "",
+                "startpage": 0,
+                "endpage": 0,
+                "startindex": 0,
+                "endindex": 0,
+                "title": "课件"
+            },
+            {
+                "id": "3",
+                "sourceType": "94",
+                "x": "393.75",
+                "y": "109.9",
+                "width": "33",
+                "height": "33",
+                "fileUrl": "source/h5/u8_p78 A Listen and match_Then act/index.html",
+                "breakpage": "",
+                "startpage": 0,
+                "endpage": 0,
+                "startindex": 0,
+                "endindex": 0,
+                "title": "练习"
+            }
+        ]
+    },
+    "Message": ""
+};
