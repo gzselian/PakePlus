57a8b832642c85fa2ab8de2ef7d18986a1139834
gzselian <3441775508@qq.com>
2025-06-11 14:51:36 +0800

    update src file from pakeplus

---
+++ src/1/page020/DialogButtonConfig.js
@@ -0,0 +1,232 @@
+var page020 = {
+	"Success": true,
+	"data": {
+		"pageContents": [
+			{
+				"contentId": 8888,
+				"sortNum": "96",
+				"x": "163.75",
+				"y": "36.75",
+				"width": "112",
+				"height": "38",
+				"audio": "source/title/Extend.mp3"
+			},
+			{
+				"contentId": 8888,
+				"sortNum": "1",
+				"x": "276.75",
+				"y": "99.75",
+				"width": "201",
+				"height": "64",
+				"audio": "source/sound/P20 Extend 01.mp3"
+			},
+			{
+				"contentId": 8888,
+				"sortNum": "2",
+				"x": "288.55",
+				"y": "165.5",
+				"width": "299",
+				"height": "64",
+				"audio": "source/sound/P20 Extend 01.mp3"
+			},
+			{
+				"contentId": 8888,
+				"sortNum": "3",
+				"x": "101.95",
+				"y": "242.9",
+				"width": "114.95",
+				"height": "30",
+				"audio": "source/sound/P20 Extend 02.mp3"
+			},
+			{
+				"contentId": 8888,
+				"sortNum": "4",
+				"x": "220.1",
+				"y": "242.9",
+				"width": "181",
+				"height": "30",
+				"audio": "source/sound/P20 Extend 03.mp3"
+			},
+			{
+				"contentId": 8888,
+				"sortNum": "5",
+				"x": "173.6",
+				"y": "277.9",
+				"width": "146",
+				"height": "30",
+				"audio": "source/sound/P20 Extend 04.mp3"
+			},
+			{
+				"contentId": 8888,
+				"sortNum": "6",
+				"x": "173.9",
+				"y": "309.05",
+				"width": "315",
+				"height": "30",
+				"audio": "source/sound/P20 Extend 05.mp3"
+			},
+			{
+				"contentId": 8888,
+				"sortNum": "7",
+				"x": "172.75",
+				"y": "339.05",
+				"width": "104",
+				"height": "30",
+				"audio": "source/sound/P20 Extend 06.mp3"
+			},
+			{
+				"contentId": 8888,
+				"sortNum": "8",
+				"x": "382.5",
+				"y": "371.55",
+				"width": "266",
+				"height": "30",
+				"audio": "source/sound/P20 Extend 07.mp3"
+			},
+			{
+				"contentId": 8888,
+				"sortNum": "9",
+				"x": "381.7",
+				"y": "402.4",
+				"width": "220",
+				"height": "30",
+				"audio": "source/sound/P20 Extend 08.mp3"
+			},
+			{
+				"contentId": 8888,
+				"sortNum": "10",
+				"x": "320.1",
+				"y": "437.9",
+				"width": "330",
+				"height": "30",
+				"audio": "source/sound/P20 Extend 09.mp3"
+			},
+			{
+				"contentId": 8888,
+				"sortNum": "11",
+				"x": "171.8",
+				"y": "636.7",
+				"width": "260",
+				"height": "30",
+				"audio": "source/sound/P20 Extend 10.mp3"
+			},
+			{
+				"contentId": 8888,
+				"sortNum": "12",
+				"x": "170.55",
+				"y": "666.7",
+				"width": "207",
+				"height": "30",
+				"audio": "source/sound/P20 Extend 11.mp3"
+			},
+			{
+				"contentId": 8888,
+				"sortNum": "13",
+				"x": "98.6",
+				"y": "701.7",
+				"width": "250",
+				"height": "30",
+				"audio": "source/sound/P20 Extend 12.mp3"
+			},
+			{
+				"contentId": 8888,
+				"sortNum": "14",
+				"x": "350.6",
+				"y": "701.7",
+				"width": "162",
+				"height": "30",
+				"audio": "source/sound/P20 Extend 13.mp3"
+			},
+			{
+				"contentId": 8888,
+				"sortNum": "15",
+				"x": "170.1",
+				"y": "750.55",
+				"width": "178",
+				"height": "30",
+				"audio": "source/sound/P20 Extend 14.mp3"
+			},
+			{
+				"contentId": 8888,
+				"sortNum": "16",
+				"x": "170.1",
+				"y": "780.55",
+				"width": "178",
+				"height": "30",
+				"audio": "source/sound/P20 Extend 15.mp3"
+			},
+			{
+				"contentId": 8888,
+				"sortNum": "17",
+				"x": "169",
+				"y": "809.3",
+				"width": "65",
+				"height": "30",
+				"audio": "source/sound/P20 Extend 16.mp3"
+			},
+			{
+				"contentId": 8888,
+				"sortNum": "18",
+				"x": "169.35",
+				"y": "836.8",
+				"width": "135",
+				"height": "30",
+				"audio": "source/sound/P20 Extend 17.mp3"
+			},
+			{
+				"contentId": 8888,
+				"sortNum": "19",
+				"x": "101.85",
+				"y": "874.2",
+				"width": "238",
+				"height": "30",
+				"audio": "source/sound/P20 Extend 18.mp3"
+			}
+		],
+		"buttons": [{
+				"id": "1",
+				"sourceType": "11",
+				"x": "283.55",
+				"y": "38.05",
+				"width": "33",
+				"height": "33",
+				"fileUrl": "source/sound/p20 Extend.mp3",
+				"breakpage": "",
+				"startpage": 0,
+				"endpage": 0,
+				"startindex": 0,
+				"endindex": 0,
+				"title": "课文录音"
+			},
+			{
+				"id": "2",
+				"sourceType": "14",
+				"x": "313.55",
+				"y": "38.05",
+				"width": "33",
+				"height": "33",
+				"fileUrl": "",
+				"startpage": 20,
+				"endpage": 20,
+				"startindex": 3,
+				"endindex": 20,
+				"title": "跟读"
+			},
+			{
+				"id": "2",
+				"sourceType": "92",
+				"x": "342.55",
+				"y": "38.05",
+				"width": "33",
+				"height": "33",
+				"fileUrl": "source/h5/u2_p20 Extend/index.html",
+				"startpage": 0,
+				"endpage": 0,
+				"startindex": 0,
+				"endindex": 0,
+				"title": "动画"
+			}
+		],
+	},
+	"Message": ""
+};
