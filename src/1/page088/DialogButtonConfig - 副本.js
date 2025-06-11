da3d1d5a6b6e0ac27062f4ae0dad0a4f23cf2570
gzselian <3441775508@qq.com>
2025-06-11 15:04:58 +0800

    update src file from pakeplus

---
+++ src/1/page088/DialogButtonConfig - 副本.js
@@ -0,0 +1,611 @@
+
+var page088 = {
+    "Success": true,
+    "data": {
+        "pageContents": [
+            {
+                "contentId": 8888,
+                "sortNum": "1",
+                "x": "311.15",
+                "y": "669.4",
+                "width": "129.35",
+                "height": "36",
+                "audio": "source/wordlist/num00.mp3"
+            },
+            {
+                "contentId": 8888,
+                "sortNum": "2",
+                "x": "162.65",
+                "y": "714.3",
+                "width": "71.4",
+                "height": "24",
+                "audio": "source/wordlist/num01.mp3"
+            },
+            {
+                "contentId": 8888,
+                "sortNum": "3",
+                "x": "162.65",
+                "y": "739.05",
+                "width": "69.9",
+                "height": "24",
+                "audio": "source/wordlist/num02.mp3"
+            },
+            {
+                "contentId": 8888,
+                "sortNum": "4",
+                "x": "162.65",
+                "y": "763.8",
+                "width": "81.4",
+                "height": "24",
+                "audio": "source/wordlist/num03.mp3"
+            },
+            {
+                "contentId": 8888,
+                "sortNum": "5",
+                "x": "162.65",
+                "y": "788.55",
+                "width": "74.9",
+                "height": "24",
+                "audio": "source/wordlist/num04.mp3"
+            },
+            {
+                "contentId": 8888,
+                "sortNum": "6",
+                "x": "162.65",
+                "y": "813.3",
+                "width": "70.9",
+                "height": "24",
+                "audio": "source/wordlist/num05.mp3"
+            },
+            {
+                "contentId": 8888,
+                "sortNum": "7",
+                "x": "162.65",
+                "y": "838.05",
+                "width": "60.4",
+                "height": "24",
+                "audio": "source/wordlist/num06.mp3"
+            },
+            {
+                "contentId": 8888,
+                "sortNum": "8",
+                "x": "162.65",
+                "y": "862.8",
+                "width": "81.4",
+                "height": "24",
+                "audio": "source/wordlist/num07.mp3"
+            },
+            {
+                "contentId": 8888,
+                "sortNum": "9",
+                "x": "162.65",
+                "y": "887.55",
+                "width": "81.4",
+                "height": "24",
+                "audio": "source/wordlist/num08.mp3"
+            },
+            {
+                "contentId": 8888,
+                "sortNum": "10",
+                "x": "162.65",
+                "y": "912.3",
+                "width": "72.9",
+                "height": "24",
+                "audio": "source/wordlist/num09.mp3"
+            },
+            {
+                "contentId": 8888,
+                "sortNum": "11",
+                "x": "162.65",
+                "y": "937.05",
+                "width": "64.4",
+                "height": "24",
+                "audio": "source/wordlist/num10.mp3"
+            },
+            {
+                "contentId": 8888,
+                "sortNum": "12",
+                "x": "482.2",
+                "y": "714.3",
+                "width": "98.45",
+                "height": "24",
+                "audio": "source/wordlist/num11.mp3"
+            },
+            {
+                "contentId": 8888,
+                "sortNum": "13",
+                "x": "482.2",
+                "y": "739.05",
+                "width": "104.4",
+                "height": "24",
+                "audio": "source/wordlist/num12.mp3"
+            },
+            {
+                "contentId": 8888,
+                "sortNum": "14",
+                "x": "482.2",
+                "y": "763.8",
+                "width": "114.9",
+                "height": "24",
+                "audio": "source/wordlist/num13.mp3"
+            },
+            {
+                "contentId": 8888,
+                "sortNum": "15",
+                "x": "482.2",
+                "y": "788.55",
+                "width": "112.9",
+                "height": "24",
+                "audio": "source/wordlist/num14.mp3"
+            },
+            {
+                "contentId": 8888,
+                "sortNum": "16",
+                "x": "482.2",
+                "y": "813.3",
+                "width": "99.45",
+                "height": "24",
+                "audio": "source/wordlist/num15.mp3"
+            },
+            {
+                "contentId": 8888,
+                "sortNum": "17",
+                "x": "482.2",
+                "y": "838.05",
+                "width": "108.45",
+                "height": "24",
+                "audio": "source/wordlist/num16.mp3"
+            },
+            {
+                "contentId": 8888,
+                "sortNum": "18",
+                "x": "482.2",
+                "y": "862.8",
+                "width": "125.4",
+                "height": "24",
+                "audio": "source/wordlist/num17.mp3"
+            },
+            {
+                "contentId": 8888,
+                "sortNum": "19",
+                "x": "482.2",
+                "y": "887.55",
+                "width": "116.4",
+                "height": "24",
+                "audio": "source/wordlist/num18.mp3"
+            },
+            {
+                "contentId": 8888,
+                "sortNum": "20",
+                "x": "482.2",
+                "y": "912.3",
+                "width": "115.4",
+                "height": "24",
+                "audio": "source/wordlist/num19.mp3"
+            },
+            {
+                "contentId": 8888,
+                "sortNum": "21",
+                "x": "482.2",
+                "y": "937.05",
+                "width": "105.45",
+                "height": "24",
+                "audio": "source/wordlist/num20.mp3"
+            },
+            {
+                "contentId": 8888,
+                "sortNum": "22",
+                "x": "37.25",
+                "y": "72.2",
+                "width": "122.35",
+                "height": "24",
+                "audio": "source/wordlist/u706.mp3"
+            },
+            {
+                "contentId": 8888,
+                "sortNum": "23",
+                "x": "37.25",
+                "y": "96.6",
+                "width": "107.5",
+                "height": "24",
+                "audio": "source/wordlist/u707.mp3"
+            },
+            {
+                "contentId": 8888,
+                "sortNum": "24",
+                "x": "37.25",
+                "y": "122",
+                "width": "102.65",
+                "height": "24",
+                "audio": "source/wordlist/u708.mp3"
+            },
+            {
+                "contentId": 8888,
+                "sortNum": "25",
+                "x": "37.25",
+                "y": "146.4",
+                "width": "125.4",
+                "height": "24",
+                "audio": "source/wordlist/u709.mp3"
+            },
+            {
+                "contentId": 8888,
+                "sortNum": "26",
+                "x": "37.25",
+                "y": "170.8",
+                "width": "66",
+                "height": "24",
+                "audio": "source/wordlist/u710.mp3"
+            },
+            {
+                "contentId": 8888,
+                "sortNum": "27",
+                "x": "37.2",
+                "y": "195.2",
+                "width": "131.55",
+                "height": "24",
+                "audio": "source/wordlist/u711.mp3"
+            },
+            {
+                "contentId": 8888,
+                "sortNum": "28",
+                "x": "37.2",
+                "y": "219.6",
+                "width": "88.45",
+                "height": "24",
+                "audio": "source/wordlist/u712.mp3"
+            },
+            {
+                "contentId": 8888,
+                "sortNum": "29",
+                "x": "37.25",
+                "y": "244",
+                "width": "93",
+                "height": "24",
+                "audio": "source/wordlist/u713.mp3"
+            },
+            {
+                "contentId": 8888,
+                "sortNum": "30",
+                "x": "37.25",
+                "y": "268.4",
+                "width": "80.1",
+                "height": "24",
+                "audio": "source/wordlist/u714.mp3"
+            },
+            {
+                "contentId": 8888,
+                "sortNum": "31",
+                "x": "37.25",
+                "y": "292.8",
+                "width": "96.65",
+                "height": "24",
+                "audio": "source/wordlist/u715.mp3"
+            },
+            {
+                "contentId": 8888,
+                "sortNum": "32",
+                "x": "37.25",
+                "y": "317.2",
+                "width": "273.9",
+                "height": "24",
+                "audio": "source/wordlist/u716.mp3"
+            },
+            {
+                "contentId": 8888,
+                "sortNum": "33",
+                "x": "37.25",
+                "y": "341.6",
+                "width": "146.85",
+                "height": "24",
+                "audio": "source/wordlist/u717.mp3"
+            },
+            {
+                "contentId": 8888,
+                "sortNum": "34",
+                "x": "37.25",
+                "y": "366",
+                "width": "173.75",
+                "height": "24",
+                "audio": "source/wordlist/u718.mp3"
+            },
+            {
+                "contentId": 8888,
+                "sortNum": "35",
+                "x": "37.2",
+                "y": "390.4",
+                "width": "169.6",
+                "height": "24",
+                "audio": "source/wordlist/u719.mp3"
+            },
+            {
+                "contentId": 8888,
+                "sortNum": "36",
+                "x": "37.2",
+                "y": "414.4",
+                "width": "196.55",
+                "height": "24",
+                "audio": "source/wordlist/u720.mp3"
+            },
+            {
+                "contentId": 8888,
+                "sortNum": "37",
+                "x": "375.2",
+                "y": "68.9",
+                "width": "67",
+                "height": "28",
+                "audio": "source/wordlist/u800.mp3"
+            },
+            {
+                "contentId": 8888,
+                "sortNum": "38",
+                "x": "374.85",
+                "y": "105.9",
+                "width": "172.9",
+                "height": "24",
+                "audio": "source/wordlist/u801.mp3"
+            },
+            {
+                "contentId": 8888,
+                "sortNum": "39",
+                "x": "374.85",
+                "y": "130.3",
+                "width": "181.4",
+                "height": "24",
+                "audio": "source/wordlist/u802.mp3"
+            },
+            {
+                "contentId": 8888,
+                "sortNum": "40",
+                "x": "374.85",
+                "y": "155.7",
+                "width": "204.35",
+                "height": "24",
+                "audio": "source/wordlist/u803.mp3"
+            },
+            {
+                "contentId": 8888,
+                "sortNum": "41",
+                "x": "374.85",
+                "y": "180.1",
+                "width": "172.7",
+                "height": "24",
+                "audio": "source/wordlist/u804.mp3"
+            },
+            {
+                "contentId": 8888,
+                "sortNum": "42",
+                "x": "374.85",
+                "y": "204.5",
+                "width": "181.45",
+                "height": "24",
+                "audio": "source/wordlist/u805.mp3"
+            },
+            {
+                "contentId": 8888,
+                "sortNum": "43",
+                "x": "374.8",
+                "y": "228.9",
+                "width": "215.05",
+                "height": "24",
+                "audio": "source/wordlist/u806.mp3"
+            },
+            {
+                "contentId": 8888,
+                "sortNum": "44",
+                "x": "374.8",
+                "y": "253.3",
+                "width": "99.3",
+                "height": "24",
+                "audio": "source/wordlist/u807.mp3"
+            },
+            {
+                "contentId": 8888,
+                "sortNum": "45",
+                "x": "374.85",
+                "y": "277.7",
+                "width": "107.6",
+                "height": "24",
+                "audio": "source/wordlist/u808.mp3"
+            },
+            {
+                "contentId": 8888,
+                "sortNum": "46",
+                "x": "374.85",
+                "y": "302.1",
+                "width": "84.55",
+                "height": "24",
+                "audio": "source/wordlist/u809.mp3"
+            },
+            {
+                "contentId": 8888,
+                "sortNum": "47",
+                "x": "374.85",
+                "y": "326.5",
+                "width": "181.4",
+                "height": "24",
+                "audio": "source/wordlist/u810.mp3"
+            },
+            {
+                "contentId": 8888,
+                "sortNum": "48",
+                "x": "374.85",
+                "y": "350.9",
+                "width": "90.85",
+                "height": "24",
+                "audio": "source/wordlist/u811.mp3"
+            },
+            {
+                "contentId": 8888,
+                "sortNum": "49",
+                "x": "374.85",
+                "y": "375.3",
+                "width": "63.15",
+                "height": "24",
+                "audio": "source/wordlist/u812.mp3"
+            },
+            {
+                "contentId": 8888,
+                "sortNum": "50",
+                "x": "374.85",
+                "y": "399.7",
+                "width": "154.25",
+                "height": "24",
+                "audio": "source/wordlist/u813.mp3"
+            },
+            {
+                "contentId": 8888,
+                "sortNum": "51",
+                "x": "374.8",
+                "y": "424.1",
+                "width": "119.55",
+                "height": "24",
+                "audio": "source/wordlist/u814.mp3"
+            },
+            {
+                "contentId": 8888,
+                "sortNum": "52",
+                "x": "374.8",
+                "y": "448.1",
+                "width": "185",
+                "height": "24",
+                "audio": "source/wordlist/u815.mp3"
+            },
+            {
+                "contentId": 8888,
+                "sortNum": "53",
+                "x": "257.9",
+                "y": "492.9",
+                "width": "236",
+                "height": "36",
+                "audio": "source/wordlist/week00.mp3"
+            },
+            {
+                "contentId": 8888,
+                "sortNum": "54",
+                "x": "130.25",
+                "y": "533.35",
+                "width": "125.4",
+                "height": "24",
+                "audio": "source/wordlist/week01.mp3"
+            },
+            {
+                "contentId": 8888,
+                "sortNum": "55",
+                "x": "130.25",
+                "y": "559.35",
+                "width": "125.4",
+                "height": "24",
+                "audio": "source/wordlist/week02.mp3"
+            },
+            {
+                "contentId": 8888,
+                "sortNum": "56",
+                "x": "130.25",
+                "y": "585.35",
+                "width": "145.9",
+                "height": "24",
+                "audio": "source/wordlist/week03.mp3"
+            },
+            {
+                "contentId": 8888,
+                "sortNum": "57",
+                "x": "130.25",
+                "y": "609.35",
+                "width": "129.9",
+                "height": "24",
+                "audio": "source/wordlist/week04.mp3"
+            },
+            {
+                "contentId": 8888,
+                "sortNum": "58",
+                "x": "482.2",
+                "y": "534.35",
+                "width": "102.9",
+                "height": "24",
+                "audio": "source/wordlist/week05.mp3"
+            },
+            {
+                "contentId": 8888,
+                "sortNum": "59",
+                "x": "482.2",
+                "y": "560.35",
+                "width": "125.4",
+                "height": "24",
+                "audio": "source/wordlist/week06.mp3"
+            },
+            {
+                "contentId": 8888,
+                "sortNum": "60",
+                "x": "482.2",
+                "y": "586.35",
+                "width": "112.9",
+                "height": "24",
+                "audio": "source/wordlist/week07.mp3"
+            }
+        ],
+        "buttons": [
+		    {
+                "id": "0",
+                "sourceType": "96",
+                "x": "153.8",
+                "y": "67",
+                "width": "33",
+                "height": "33",
+                "fileUrl": "source/wordlist/Unit702.mp3",
+                "breakpage": "",
+                "startpage": 0,
+                "endpage": 0,
+                "startindex": 0,
+                "endindex": 0,
+                "title": "按钮标题"
+            },
+            {
+                "id": "1",
+                "sourceType": "96",
+                "x": "454.45",
+                "y": "672.4",
+                "width": "33",
+                "height": "33",
+                "fileUrl": "source/wordlist/Numbers.mp3",
+                "breakpage": "",
+                "startpage": 0,
+                "endpage": 0,
+                "startindex": 0,
+                "endindex": 0,
+                "title": "按钮标题"
+            },
+            {
+                "id": "2",
+                "sourceType": "96",
+                "x": "499.4",
+                "y": "492.9",
+                "width": "33",
+                "height": "33",
+                "fileUrl": "source/wordlist/Daysoftheweek.mp3",
+                "breakpage": "",
+                "startpage": 0,
+                "endpage": 0,
+                "startindex": 0,
+                "endindex": 0,
+                "title": "按钮标题"
+            },
+            {
+                "id": "3",
+                "sourceType": "96",
+                "x": "449.2",
+                "y": "65.9",
+                "width": "33",
+                "height": "33",
+                "fileUrl": "source/wordlist/Unit8.mp3",
+                "breakpage": "",
+                "startpage": 0,
+                "endpage": 0,
+                "startindex": 0,
+                "endindex": 0,
+                "title": "按钮标题"
+            }
+        ]
+    },
+    "Message": ""
+};
