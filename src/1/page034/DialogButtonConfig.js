eaf57498b58db9831d36f7890bce6fbfc26ad503
gzselian <3441775508@qq.com>
2025-06-11 14:54:35 +0800

    update src file from pakeplus

---
+++ src/1/page034/DialogButtonConfig.js
@@ -0,0 +1,142 @@
+var page034 = {
+	"Success": true,
+	"data": {
+		"pageContents": [
+			{
+				"contentId": 8888,
+				"sortNum": "98",
+				"x": "160.4",
+				"y": "38.35",
+				"width": "120",
+				"height": "36",
+				"audio": "source/title/Explore.mp3"
+			},{
+                "contentId": 8888,
+                "sortNum": "1",
+                "x": "384.6",
+                "y": "57.95",
+                "width": "309",
+                "height": "62",
+                "audio": "source/sound/p34 Explore 01.mp3"
+            },
+            {
+                "contentId": 8888,
+                "sortNum": "2",
+                "x": "62.7",
+                "y": "170.3",
+                "width": "213",
+                "height": "59",
+                "audio": "source/sound/p34 Explore 02.mp3"
+            },
+            {
+                "contentId": 8888,
+                "sortNum": "3",
+                "x": "62.7",
+                "y": "231.25",
+                "width": "213",
+                "height": "24",
+                "audio": "source/sound/p34 Explore 03.mp3"
+            },
+            {
+                "contentId": 8888,
+                "sortNum": "4",
+                "x": "62.7",
+                "y": "257.25",
+                "width": "213",
+                "height": "24",
+                "audio": "source/sound/p34 Explore 04.mp3"
+            },
+            {
+                "contentId": 8888,
+                "sortNum": "5",
+                "x": "66.7",
+                "y": "349.85",
+                "width": "238.95",
+                "height": "54",
+                "audio": "source/sound/p34 Explore 05.mp3"
+            },
+            {
+                "contentId": 8888,
+                "sortNum": "6",
+                "x": "66.7",
+                "y": "407.2",
+                "width": "238.95",
+                "height": "57",
+                "audio": "source/sound/p34 Explore 06.mp3"
+            },
+            {
+                "contentId": 8888,
+                "sortNum": "7",
+                "x": "381.6",
+                "y": "813.1",
+                "width": "208",
+                "height": "59",
+                "audio": "source/sound/p34 Explore 07.mp3"
+            },
+            {
+                "contentId": 8888,
+                "sortNum": "8",
+                "x": "378.6",
+                "y": "874.1",
+                "width": "213",
+                "height": "24",
+                "audio": "source/sound/p34 Explore 08.mp3"
+            },
+            {
+                "contentId": 8888,
+                "sortNum": "9",
+                "x": "378.6",
+                "y": "900.1",
+                "width": "213",
+                "height": "24",
+                "audio": "source/sound/p34 Explore 09.mp3"
+            }
+		],
+		"buttons": [{
+				"id": "1",
+				"sourceType": "11",
+				"x": "290.9",
+				"y": "38",
+				"width": "33",
+				"height": "33",
+				"fileUrl": "source/sound/p34 Explore.mp3",
+				"breakpage": "",
+				"startpage": 0,
+				"endpage": 0,
+				"startindex": 0,
+				"endindex": 0,
+				"title": "课文录音"
+			},
+			{
+				"id": "2",
+				"sourceType": "14",
+				"x": "317",
+				"y": "38",
+				"width": "33",
+				"height": "33",
+				"breakpage": "",
+				"startpage": 34,
+				"endpage": 35,
+				"startindex": 2,
+				"endindex": 7,
+				"title": "跟读"
+			},
+			{
+				"id": "3",
+				"sourceType": "92",
+				"x": "347",
+				"y": "38",
+				"width": "33",
+				"height": "33",
+				"fileUrl": "source/h5/u4_p34 Explore/index.html",
+				"breakpage": "",
+				"startpage": 0,
+				"endpage": 0,
+				"startindex": 0,
+				"endindex": 0,
+				"title": "动画"
+			}
+		]
+	},
+	"Message": ""
+};
