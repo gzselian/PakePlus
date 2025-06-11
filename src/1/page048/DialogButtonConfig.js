9711e5a9dc7d65c282eb077754957c1e0ca7d0d5
gzselian <3441775508@qq.com>
2025-06-11 14:57:13 +0800

    update src file from pakeplus

---
+++ src/1/page048/DialogButtonConfig.js
@@ -0,0 +1,74 @@
+
+var page048 = {
+    "Success": true,
+    "data": {
+        "pageContents": [
+			{
+			    "contentId": 8888,
+			    "sortNum": "97",
+			    "x": "151.55",
+			    "y": "37",
+			    "width": "196",
+			    "height": "38",
+			    "audio": "source/title/Communicate.mp3"
+			},
+			{
+                "contentId": 8888,
+                "sortNum": "1",
+                "x": "59.95",
+                "y": "111.9",
+                "width": "324",
+                "height": "32",
+                "audio": "source/title/Listen and number then talk.mp3"
+            }
+        ],
+        "buttons": [
+            {
+                "id": "1",
+                "sourceType": "11",
+                "x": "385.75",
+                "y": "109.9",
+                "width": "33",
+                "height": "33",
+                "fileUrl": "source/sound/p48 A Listen and number_Then talk.mp3",
+                "breakpage": "",
+                "startpage": 0,
+                "endpage": 0,
+                "startindex": 0,
+                "endindex": 0,
+                "title": "课文录音"
+            },
+            {
+                "id": "2",
+                "sourceType": "93",
+                "x": "438.75",
+                "y": "109.9",
+                "width": "33",
+                "height": "33",
+                "fileUrl": "source/h5/u5_p48 A Listen and number_Then talk02/index.html",
+                "breakpage": "",
+                "startpage": 0,
+                "endpage": 0,
+                "startindex": 0,
+                "endindex": 0,
+                "title": "课件"
+            },
+            {
+                "id": "3",
+                "sourceType": "94",
+                "x": "412.75",
+                "y": "109.9",
+                "width": "33",
+                "height": "33",
+                "fileUrl": "source/h5/u5_p48 A Listen and number_Then talk/index.html",
+                "breakpage": "",
+                "startpage": 0,
+                "endpage": 0,
+                "startindex": 0,
+                "endindex": 0,
+                "title": "练习"
+            }
+        ]
+    },
+    "Message": ""
+};
