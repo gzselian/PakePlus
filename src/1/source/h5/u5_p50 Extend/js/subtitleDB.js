73e92b2d2ff8002c6e11c71d903173093bd9c0ce
gzselian <3441775508@qq.com>
2025-06-11 18:12:03 +0800

    update src file from pakeplus

---
+++ src/1/source/h5/u5_p50 Extend/js/subtitleDB.js
@@ -0,0 +1,54 @@
+var movPath="../../mov/P50 Extend.mp4";
+var soundPath="source/01bg.mp3";
+var movRatio="16:9";
+
+var subtitleJson = {
+"wordChArr": [
+    "我叫骆宾王。",
+    "我七岁。",
+    "这是我的诗。",
+    "你喜欢吗？",
+	
+    "鹅！鹅！鹅！",
+    "看它们的长脖子。",
+    "他们朝着天空唱歌。",
+    "白色的羽毛，绿色的水，红色的脚掌。",
+    "真美！真美！真美！"
+],
+"wordArr": [
+    "My name is Luo Binwang. ",
+    "I’m seven.",
+    "This is my poem.",
+    "Do you like it?",
+    "Geese! Geese! Geese!",
+    "Look at their long necks.",
+    "They sing songs into the sky.",
+    "White feathers, green water, red feet.",
+    "Nice! Nice! Nice!"
+],
+"timeArr": [
+"875", "4708",
+"5166", "6875",
+"7458", "9875",
+"10416", "12500",
+"24583", "28375",
+"28750", "32041",
+"32791", "36541",
+"37041", "42333",
+"42833", "46666"
+], // 9
+"rolepoints": [
+    "l", "l", "l", "l", 
+	"a", "a","a", "a", "a"
+],
+// 齐声
+// 1:05.349-1:07.919
+// 角色出现顺序
+"allrole":
+     [ "l","a"]
+};
+var rolelist=[ "l","a"];
+
+var Allrolepoints = [];
+
+ 
