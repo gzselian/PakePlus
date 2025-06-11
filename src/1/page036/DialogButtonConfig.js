6d773925d1eafe162d95caa93655fc03ae53a271
gzselian <3441775508@qq.com>
2025-06-11 14:55:00 +0800

    update src file from pakeplus

---
+++ src/1/page036/DialogButtonConfig.js
@@ -0,0 +1,41 @@
+
+var page036 = {
+    "Success": true,
+    "data": {
+        "pageContents": [
+        ],
+        "buttons": [
+            {
+                "id": "1",
+                "sourceType": "93",
+                "x": "625.75",
+                "y": "66",
+                "width": "33",
+                "height": "33",
+                "fileUrl": "source/h5/u4_p36 A What is your favourite part of the poem Tick and say/index.html",
+                "breakpage": "",
+                "startpage": 0,
+                "endpage": 0,
+                "startindex": 0,
+                "endindex": 0,
+                "title": "课件"
+            },
+            {
+                "id": "2",
+                "sourceType": "93",
+                "x": "231.75",
+                "y": "498",
+                "width": "33",
+                "height": "33",
+                "fileUrl": "source/h5/u4_p36 B Read and act/index.html",
+                "breakpage": "",
+                "startpage": 0,
+                "endpage": 0,
+                "startindex": 0,
+                "endindex": 0,
+                "title": "练习"
+            }
+        ]
+    },
+    "Message": ""
+};
