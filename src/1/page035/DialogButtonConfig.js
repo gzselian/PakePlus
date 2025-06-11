71f8b004bf342c89eabf3f0b7d8f30b572bd9d43
gzselian <3441775508@qq.com>
2025-06-11 14:54:47 +0800

    update src file from pakeplus

---
+++ src/1/page035/DialogButtonConfig.js
@@ -0,0 +1,97 @@
+var page035 = {
+    "Success": true,
+    "data": {
+        "pageContents": [
+            {
+                "contentId": 8888,
+                "sortNum": "10",
+                "x": "435.8",
+                "y": "48.6",
+                "width": "256",
+                "height": "59",
+                "audio": "source/sound/P34 Explore 10.mp3"
+            },
+            {
+                "contentId": 8888,
+                "sortNum": "11",
+                "x": "435.8",
+                "y": "107.6",
+                "width": "256",
+                "height": "59",
+                "audio": "source/sound/p34 Explore 11.mp3"
+            },
+            {
+                "contentId": 8888,
+                "sortNum": "12",
+                "x": "397.8",
+                "y": "330.15",
+                "width": "238.95",
+                "height": "59",
+                "audio": "source/sound/p34 Explore 12.mp3"
+            },
+            {
+                "contentId": 8888,
+                "sortNum": "13",
+                "x": "397.6",
+                "y": "391.55",
+                "width": "218",
+                "height": "24",
+                "audio": "source/sound/p34 Explore 13.mp3"
+            },
+            {
+                "contentId": 8888,
+                "sortNum": "14",
+                "x": "397.6",
+                "y": "417.55",
+                "width": "218",
+                "height": "24",
+                "audio": "source/sound/p34 Explore 14.mp3"
+            },
+            {
+                "contentId": 8888,
+                "sortNum": "15",
+                "x": "430.8",
+                "y": "639.85",
+                "width": "194.95",
+                "height": "58",
+                "audio": "source/sound/p34 Explore 15.mp3"
+            },
+            {
+                "contentId": 8888,
+                "sortNum": "16",
+                "x": "430.8",
+                "y": "701.85",
+                "width": "194.95",
+                "height": "58",
+                "audio": "source/sound/p34 Explore 16.mp3"
+            },
+			{
+                "contentId": 8888,
+                "sortNum": "1",
+                "x": "551.4",
+                "y": "810.9",
+                "width": "59",
+                "height": "56",
+                "audio": "source/sound/P35 Letters and sounds.mp3"
+            }
+        ],
+        "buttons": [
+		{
+                "id": "1",
+                "sourceType": "92",
+                "x": "691",
+                "y": "832",
+                "width": "40",
+                "height": "40",
+                "fileUrl": "source/h5/u4_p35 Sounds/index.html",
+                "breakpage": "",
+                "startpage": 0,
+                "endpage": 0,
+                "startindex": 0,
+                "endindex": 0,
+                "title": "动画"
+            }
+        ]
+    },
+    "Message": ""
+};
