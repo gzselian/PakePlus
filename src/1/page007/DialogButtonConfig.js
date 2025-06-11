38ab389134feccf194f9acee533feb6e4a97e77b
gzselian <3441775508@qq.com>
2025-06-11 14:48:36 +0800

    update src file from pakeplus

---
+++ src/1/page007/DialogButtonConfig.js
@@ -0,0 +1,184 @@
+var page007 = {
+	"Success": true,
+	"data": {
+		"pageContents": [
+			{
+				"contentId": 8888,
+				"sortNum": "0",
+				"x": "316.6",
+				"y": "106.65",
+				"width": "113",
+				"height": "33",
+				"audio": "source/sound/p7 C Listen and chant 01.mp3"
+			},
+			{
+				"contentId": 8888,
+				"sortNum": "1",
+				"x": "65.6",
+				"y": "150.9",
+				"width": "326",
+				"height": "27.95",
+				"audio": "source/sound/p7 C Listen and chant 02.mp3"
+			},
+			{
+				"contentId": 8888,
+				"sortNum": "2",
+				"x": "65.6",
+				"y": "178.85",
+				"width": "280",
+				"height": "27.95",
+				"audio": "source/sound/p7 C Listen and chant 03.mp3"
+			},
+			{
+				"contentId": 8888,
+				"sortNum": "3",
+				"x": "65.6",
+				"y": "208.8",
+				"width": "253",
+				"height": "27.95",
+				"audio": "source/sound/p7 C Listen and chant 04.mp3"
+			},
+			
+			{
+				"contentId": 8888,
+				"sortNum": "4",
+				"x": "65.6",
+				"y": "238.75",
+				"width": "253",
+				"height": "27.95",
+				"audio": "source/sound/p7 C Listen and chant 05.mp3"
+			},
+			{
+				"contentId": 8888,
+				"sortNum": "5",
+				"x": "65.6",
+				"y": "275.8",
+				"width": "317",
+				"height": "27.95",
+				"audio": "source/sound/p7 C Listen and chant 06.mp3"
+			},
+			{
+				"contentId": 8888,
+				"sortNum": "6",
+				"x": "65.6",
+				"y": "305.8",
+				"width": "296",
+				"height": "27.95",
+				"audio": "source/sound/p7 C Listen and chant 07.mp3"
+			},
+			{
+				"contentId": 8888,
+				"sortNum": "7",
+				"x": "65.6",
+				"y": "334.75",
+				"width": "316.95",
+				"height": "27.95",
+				"audio": "source/sound/p7 C Listen and chant 08.mp3"
+			},
+			{
+				"contentId": 8888,
+				"sortNum": "8",
+				"x": "65.6",
+				"y": "362.7",
+				"width": "349",
+				"height": "27.95",
+				"audio": "source/sound/p7 C Listen and chant 09.mp3"
+			},
+			 {
+			                "contentId": 8888,
+			                "sortNum": "9",
+			                "x": "97.15",
+			                "y": "463.35",
+			                "width": "258",
+			                "height": "30",
+			                "audio": "source/sound/p7 Learn 01.mp3"
+			            },
+			            {
+			                "contentId": 8888,
+			                "sortNum": "10",
+			                "x": "96.45",
+			                "y": "494.35",
+			                "width": "142",
+			                "height": "30",
+			                "audio": "source/sound/p7 Learn 02.mp3"
+			            },{
+                "contentId": 8888,
+                "sortNum": "1",
+                "x": "55.65",
+                "y": "70.95",
+                "width": "206",
+                "height": "36",
+                "audio": "source/title/C Listen and chant.mp3"
+            },
+			{
+                "contentId": 8888,
+                "sortNum": "1",
+                "x": "128.05",
+                "y": "425.9",
+                "width": "66",
+                "height": "32",
+                "audio": "source/title/Learn.mp3"
+            }
+		],
+		"buttons": [{
+				"id": "1",
+				"sourceType": "11",
+				"x": "258.95",
+				"y": "71.75",
+				"width": "33",
+				"height": "33",
+				"fileUrl": "source/sound/p7 C Listen and chant.mp3",
+				"breakpage": "",
+				"startpage": 0,
+				"endpage": 0,
+				"startindex": 0,
+				"endindex": 0,
+				"title": "课文录音"
+			},
+			{
+				"id": "2",
+				"sourceType": "93",
+				"x": "288.95",
+				"y": "71.75",
+				"width": "33",
+				"height": "33",
+				"fileUrl": "source/h5/u1_p7 C Listen and chant/index.html",
+				"startpage": 0,
+				"endpage": 0,
+				"startindex": 0,
+				"endindex": 0,
+				"title": "课件"
+			},
+			{
+				"id": "3",
+				"sourceType": "93",
+				"x": "201",
+				"y": "426.8",
+				"width": "33",
+				"height": "33",
+				"fileUrl": "source/h5/u1_p7 Learn/index.html",
+				"startpage": 0,
+				"endpage": 0,
+				"startindex": 0,
+				"endindex": 0,
+				"title": "课件"
+			},
+			{
+			                "id": "3",
+			                "sourceType": "93",
+			                "x": "233",
+			                "y": "583",
+			                "width": "33",
+			                "height": "33",
+			                "fileUrl": "source/h5/u1_p7 D Look and talk/index.html",
+			                "breakpage": "",
+			                "startpage": 0,
+			                "endpage": 0,
+			                "startindex": 0,
+			                "endindex": 0,
+			                "title": "练习"
+			            }
+		],
+	},
+	"Message": ""
+};
