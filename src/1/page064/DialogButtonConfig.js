155cb629bb2f4da120bed4f49f374cb8ea5c112c
gzselian <3441775508@qq.com>
2025-06-11 15:00:29 +0800

    update src file from pakeplus

---
+++ src/1/page064/DialogButtonConfig.js
@@ -0,0 +1,252 @@
+var page064 = {
+	"Success": true,
+	"data": {
+		"pageContents": [
+			{
+				"contentId": 8888,
+				"sortNum": "98",
+				"x": "160.4",
+				"y": "38.35",
+				"width": "120",
+				"height": "36",
+				"audio": "source/title/Explore.mp3"
+			},
+			{
+                "contentId": 8888,
+                "sortNum": "1",
+                "x": "301.6",
+                "y": "173.35",
+                "width": "391",
+                "height": "64",
+                "audio": "source/sound/p64 Explore 01.mp3"
+            },
+            {
+                "contentId": 8888,
+                "sortNum": "2",
+                "x": "448.4",
+                "y": "257.15",
+                "width": "147",
+                "height": "27",
+                "audio": "source/sound/p64 Explore 02.mp3"
+            },
+            {
+                "contentId": 8888,
+                "sortNum": "3",
+                "x": "595.4",
+                "y": "257.15",
+                "width": "97",
+                "height": "27",
+                "audio": "source/sound/p64 Explore 03.mp3"
+            },
+            {
+                "contentId": 8888,
+                "sortNum": "3",
+                "x": "448.7",
+                "y": "284.15",
+                "width": "93.7",
+                "height": "25",
+                "audio": "source/sound/p64 Explore 03.mp3"
+            },
+            {
+                "contentId": 8888,
+                "sortNum": "4",
+                "x": "544.4",
+                "y": "284.15",
+                "width": "145.95",
+                "height": "25",
+                "audio": "source/sound/p64 Explore 04.mp3"
+            },
+            {
+                "contentId": 8888,
+                "sortNum": "4",
+                "x": "449.1",
+                "y": "311.15",
+                "width": "180.95",
+                "height": "25",
+                "audio": "source/sound/p64 Explore 04.mp3"
+            },
+            {
+                "contentId": 8888,
+                "sortNum": "5",
+                "x": "630.05",
+                "y": "311.15",
+                "width": "60.3",
+                "height": "25",
+                "audio": "source/sound/p64 Explore 05.mp3"
+            },
+            {
+                "contentId": 8888,
+                "sortNum": "5",
+                "x": "450.7",
+                "y": "336.15",
+                "width": "93.3",
+                "height": "25",
+                "audio": "source/sound/p64 Explore 05.mp3"
+            },
+            {
+                "contentId": 8888,
+                "sortNum": "6",
+                "x": "450.1",
+                "y": "404.15",
+                "width": "197.95",
+                "height": "25",
+                "audio": "source/sound/p64 Explore 06.mp3"
+            },
+            {
+                "contentId": 8888,
+                "sortNum": "7",
+                "x": "450.1",
+                "y": "431.15",
+                "width": "230.95",
+                "height": "25",
+                "audio": "source/sound/p64 Explore 07.mp3"
+            },
+            {
+                "contentId": 8888,
+                "sortNum": "8",
+                "x": "450.1",
+                "y": "465.1",
+                "width": "138.95",
+                "height": "25",
+                "audio": "source/sound/p64 Explore 08.mp3"
+            },
+            {
+                "contentId": 8888,
+                "sortNum": "9",
+                "x": "450.1",
+                "y": "497.1",
+                "width": "230.95",
+                "height": "25",
+                "audio": "source/sound/p64 Explore 09.mp3"
+            },
+            {
+                "contentId": 8888,
+                "sortNum": "9",
+                "x": "450.1",
+                "y": "524.1",
+                "width": "68.95",
+                "height": "25",
+                "audio": "source/sound/p64 Explore 09.mp3"
+            },
+            {
+                "contentId": 8888,
+                "sortNum": "10",
+                "x": "450.1",
+                "y": "593.1",
+                "width": "230.95",
+                "height": "25",
+                "audio": "source/sound/p64 Explore 10.mp3"
+            },
+            {
+                "contentId": 8888,
+                "sortNum": "10",
+                "x": "450.1",
+                "y": "620.1",
+                "width": "144.95",
+                "height": "25",
+                "audio": "source/sound/p64 Explore 10.mp3"
+            },
+            {
+                "contentId": 8888,
+                "sortNum": "11",
+                "x": "595.05",
+                "y": "620.1",
+                "width": "77.95",
+                "height": "25",
+                "audio": "source/sound/p64 Explore 11.mp3"
+            },
+            {
+                "contentId": 8888,
+                "sortNum": "11",
+                "x": "450.65",
+                "y": "645.1",
+                "width": "220.95",
+                "height": "25",
+                "audio": "source/sound/p64 Explore 11.mp3"
+            },
+            {
+                "contentId": 8888,
+                "sortNum": "11",
+                "x": "450.65",
+                "y": "670.1",
+                "width": "77.95",
+                "height": "25",
+                "audio": "source/sound/p64 Explore 11.mp3"
+            },
+            {
+                "contentId": 8888,
+                "sortNum": "12",
+                "x": "450.65",
+                "y": "703.05",
+                "width": "135.95",
+                "height": "25",
+                "audio": "source/sound/p64 Explore 12.mp3"
+            },
+            {
+                "contentId": 8888,
+                "sortNum": "13",
+                "x": "588.6",
+                "y": "703.05",
+                "width": "102.95",
+                "height": "25",
+                "audio": "source/sound/p64 Explore 13.mp3"
+            },
+            {
+                "contentId": 8888,
+                "sortNum": "13",
+                "x": "450.65",
+                "y": "731.05",
+                "width": "242.95",
+                "height": "25",
+                "audio": "source/sound/p64 Explore 13.mp3"
+            }
+			
+		],
+		"buttons": [{
+				"id": "1",
+				"sourceType": "11",
+				"x": "288.75",
+				"y": "37.9",
+				"width": "33",
+				"height": "33",
+				"fileUrl": "source/sound/p64 Explore.mp3",
+				"breakpage": "",
+				"startpage": 0,
+				"endpage": 0,
+				"startindex": 0,
+				"endindex": 0,
+				"title": "课文录音"
+			},
+			{
+				"id": "2",
+				"sourceType": "14",
+				"x": "321.75",
+				"y": "37.9",
+				"width": "33",
+				"height": "33",
+				"breakpage": "",
+				"startpage": 64,
+				"endpage": 65,
+				"startindex": 2,
+				"endindex": 15,
+				"title": "跟读"
+			},
+			{
+				"id": "3",
+				"sourceType": "92",
+				"x": "354.75",
+				"y": "37.9",
+				"width": "33",
+				"height": "33",
+				"fileUrl": "source/h5/u7_p64 Explore/index.html",
+				"breakpage": "",
+				"startpage": 0,
+				"endpage": 0,
+				"startindex": 0,
+				"endindex": 0,
+				"title": "动画"
+			}
+		]
+	},
+	"Message": ""
+};
