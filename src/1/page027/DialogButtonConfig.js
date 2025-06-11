3767780cd77009d602c762bb5dc3754b1450232d
gzselian <3441775508@qq.com>
2025-06-11 14:53:18 +0800

    update src file from pakeplus

---
+++ src/1/page027/DialogButtonConfig.js
@@ -0,0 +1,190 @@
+var page027 = {
+	"Success": true,
+	"data": {
+		"pageContents": [{
+				"contentId": 8888,
+				"sortNum": "0",
+				"x": "282.1",
+				"y": "103",
+				"width": "170",
+				"height": "30",
+				"audio": "source/sound/p27 C Listen and chant 01.mp3"
+			}, {
+				"contentId": 8888,
+				"sortNum": "1",
+				"x": "52.05",
+				"y": "148",
+				"width": "318",
+				"height": "30",
+				"audio": "source/sound/p27 C Listen and chant 02.mp3"
+			},
+			{
+				"contentId": 8888,
+				"sortNum": "2",
+				"x": "52.05",
+				"y": "178",
+				"width": "318",
+				"height": "30",
+				"audio": "source/sound/p27 C Listen and chant 03.mp3"
+			},
+			{
+				"contentId": 8888,
+				"sortNum": "3",
+				"x": "52.05",
+				"y": "208",
+				"width": "318",
+				"height": "30",
+				"audio": "source/sound/p27 C Listen and chant 04.mp3"
+			},
+			{
+				"contentId": 8888,
+				"sortNum": "4",
+				"x": "52.05",
+				"y": "237",
+				"width": "318",
+				"height": "30",
+				"audio": "source/sound/p27 C Listen and chant 05.mp3"
+			},
+			{
+				"contentId": 8888,
+				"sortNum": "5",
+				"x": "73.8",
+				"y": "267",
+				"width": "109",
+				"height": "30",
+				"audio": "source/sound/p27 C Listen and chant 05.mp3"
+			},
+			{
+				"contentId": 8888,
+				"sortNum": "6",
+				"x": "392",
+				"y": "148",
+				"width": "306",
+				"height": "30",
+				"audio": "source/sound/p27 C Listen and chant 06.mp3"
+			},
+			{
+				"contentId": 8888,
+				"sortNum": "7",
+				"x": "392",
+				"y": "178",
+				"width": "306",
+				"height": "30",
+				"audio": "source/sound/p27 C Listen and chant 07.mp3"
+			},
+			{
+				"contentId": 8888,
+				"sortNum": "8",
+				"x": "392",
+				"y": "208",
+				"width": "306",
+				"height": "30",
+				"audio": "source/sound/p27 C Listen and chant 08.mp3"
+			},
+			{
+				"contentId": 8888,
+				"sortNum": "9",
+				"x": "392",
+				"y": "237",
+				"width": "306",
+				"height": "30",
+				"audio": "source/sound/p27 C Listen and chant 09.mp3"
+			},
+			{
+				"contentId": 8888,
+				"sortNum": "10",
+				"x": "406.25",
+				"y": "267",
+				"width": "85",
+				"height": "30",
+				"audio": "source/sound/p27 C Listen and chant 09.mp3"
+			},
+			{
+				"contentId": 8888,
+				"sortNum": "11",
+				"x": "95.45",
+				"y": "401.1",
+				"width": "437",
+				"height": "63",
+				"audio": "source/sound/p27 Learn.mp3"
+			},{
+                "contentId": 8888,
+                "sortNum": "1",
+                "x": "55.65",
+                "y": "70.95",
+                "width": "206",
+                "height": "36",
+                "audio": "source/title/C Listen and chant.mp3"
+            },
+			{
+                "contentId": 8888,
+                "sortNum": "1",
+                "x": "128.05",
+                "y": "363.9",
+                "width": "66",
+                "height": "32",
+                "audio": "source/title/Learn.mp3"
+            }
+
+		],
+		"buttons": [{
+				"id": "1",
+				"sourceType": "11",
+				"x": "258.6",
+				"y": "71.95",
+				"width": "33",
+				"height": "33",
+				"fileUrl": "source/sound/p27 C Listen and chant.mp3",
+				"breakpage": "",
+				"startpage": 0,
+				"endpage": 0,
+				"startindex": 0,
+				"endindex": 0,
+				"title": "课文录音"
+			},
+			{
+				"id": "2",
+				"sourceType": "93",
+				"x": "288.6",
+				"y": "71.95",
+				"width": "33",
+				"height": "33",
+				"fileUrl": "source/h5/u3_p27 C Listen and chant/index.html",
+				"startpage": 0,
+				"endpage": 0,
+				"startindex": 0,
+				"endindex": 0,
+				"title": "课件"
+			},
+			{
+				"id": "3",
+				"sourceType": "93",
+				"x": "207.15",
+				"y": "365",
+				"width": "33",
+				"height": "33",
+				"fileUrl": "source/h5/u3_p27 Learn/index.html",
+				"startpage": 0,
+				"endpage": 0,
+				"startindex": 0,
+				"endindex": 0,
+				"title": "课件"
+			},
+			/* {
+				"id": "4",
+				"sourceType": "93",
+				"x": "260.15",
+				"y": "510.95",
+				"width": "33",
+				"height": "33",
+				"fileUrl": "source/h5/u3_p27 D Ask and answer/index.html",
+				"startpage": 0,
+				"endpage": 0,
+				"startindex": 0,
+				"endindex": 0,
+				"title": "课件"
+			} */
+		],
+	},
+	"Message": ""
+};
