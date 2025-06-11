fbf3cdf7a87dd443cde76f9652680ea616589ecf
gzselian <3441775508@qq.com>
2025-06-11 14:59:56 +0800

    update src file from pakeplus

---
+++ src/1/page061/DialogButtonConfig.js
@@ -0,0 +1,35 @@
+
+var page061 = {
+    "Success": true,
+    "data": {
+        "pageContents": [
+            {
+                "contentId": 8888,
+                "sortNum": "1",
+                "x": "116.8",
+                "y": "112.6",
+                "width": "411.95",
+                "height": "50",
+                "audio": "source/sound/p61 What do we do at school.mp3"
+            }
+        ],
+        "buttons": [
+		{
+				"id": "0",
+				"sourceType": "92",
+				"x": "680",
+				"y": "910",
+				"width": "60",
+				"height": "60",
+				"fileUrl": "source/mov/daor/u7_daor.mp4",
+				// "breakpage":"33,6",
+				"startpage": 0,
+				"endpage": 0,
+				"startindex": 0,
+				"endindex": 0,
+				"title": "导入视频"
+			}
+        ]
+    },
+    "Message": ""
+};
