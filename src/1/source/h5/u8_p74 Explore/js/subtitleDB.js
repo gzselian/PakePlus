afd6f1e1ed8fae25f19d35a7f397a1febc220ced
gzselian <3441775508@qq.com>
2025-06-11 19:01:31 +0800

    update src file from pakeplus

---
+++ src/1/source/h5/u8_p74 Explore/js/subtitleDB.js
@@ -0,0 +1,88 @@
+var movPath="../../mov/P74 Explore.mp4";
+// var movPath="source/08.mp4";
+var soundPath="source/01bg.mp3";
+var movRatio="16:9";
+
+var subtitleJson = {
+"wordChArr": [
+    "浩浩正在进行一项",
+	"关于儿童节的调查。",
+    "你在儿童节做什么？",
+    "我去科学博物馆。",
+    "这很有趣！",
+    "亮亮，你在儿童节做什么？",
+    "我们全家去动物园。",
+    "我喜欢大象。",
+    "轩轩，你呢？",
+    "我们全家去看电影。",
+    "我喜欢《孙悟空》。",
+    "哈！哈！",
+    "看完电影，我们去了一家披萨店。",
+    "有时候我吃得太多了！",
+    "在我们班，",
+    "三个同学在儿童节去动物园。",
+    "四个同学去看电影。",
+    "三个同学去博物馆。",
+    "十个同学出去吃饭！"
+],
+"wordArr": [
+    "Haohao is doing a survey",
+	"about Children’s Day.",
+    "What do you do on Children’s Day?",
+    "I go to the science museum.",
+    "It’s fun!",
+    "Liangliang, what do you do on Children’s Day?",
+    "My family go to the zoo.",
+    "I love elephants.",
+    "Xuanxuan, what about you?",
+    "My family go to the cinema.",
+    "I like Monkey King.",
+    "Ha! Ha!",
+    "After the film, we go to a pizza shop.",
+    "Sometimes I eat too much!",
+    "In our class,",
+    "three classmates go to the zoo on Children’s Day.",
+    "Four classmates go to the cinema.",
+    "Three classmates go to the museum.",
+    "Ten classmates go out to eat!"
+   
+],
+"timeArr": [
+"5583", "9041",
+"9125", "11625",
+"12125", "15666",
+"16125", "20333",
+"20375", "22458",
+"23250", "28375",
+"29041", "32666",
+"32750", "35083",
+"36750", "39916",
+"40708", "44250",
+"44375", "47083",
+"49041", "50708",
+"51666", "56416",
+"57125", "60416",
+"63166", "65083",
+"65291", "69875",
+//"68083", "69875",
+"70041", "73416",
+"73958", "77458",
+"78000", "81833"
+], // 20
+
+// 齐声
+// 1:05.349-1:07.919
+// 角色出现顺序
+"rolepoints": [
+    "pangbai","pangbai","Haohao", "Fanfa", "Fanfa", "Haohao", "Liangliang",
+	"Liangliang","Haohao","Xuanxuan", "Xuanxuan", "Haohao","Xuanxuan", 
+	"Xuanxuan", "Haohao","Haohao", "Haohao", "Haohao", "Haohao","Haohao"
+	],
+"allrole":
+     [ "pangbai", "Haohao", "Fanfa","Liangliang","Xuanxuan"]
+};
+var rolelist=["Teacher","Xiaoyuan","Leilei","Xiaolan","Congcong"];
+
+var Allrolepoints = [];
+
+ 
