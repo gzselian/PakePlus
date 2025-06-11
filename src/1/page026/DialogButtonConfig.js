ea89059c3f073d5af2773c4c71da21a2ca234260
gzselian <3441775508@qq.com>
2025-06-11 14:53:08 +0800

    update src file from pakeplus

---
+++ src/1/page026/DialogButtonConfig.js
@@ -0,0 +1,41 @@
+
+var page026 = {
+    "Success": true,
+    "data": {
+        "pageContents": [
+        ],
+        "buttons": [
+            {
+                "id": "1",
+                "sourceType": "93",
+                "x": "489.75",
+                "y": "65.9",
+                "width": "33",
+                "height": "33",
+                "fileUrl": "source/h5/u3_p26 A Whose clothes do you like Tick and say/index.html",
+                "breakpage": "",
+                "startpage": 0,
+                "endpage": 0,
+                "startindex": 0,
+                "endindex": 0,
+                "title": "课件"
+            },
+            {
+                "id": "2",
+                "sourceType": "94",
+                "x": "270.75",
+                "y": "518.85",
+                "width": "33",
+                "height": "33",
+                "fileUrl": "source/h5/u3_p26 B Read and choose/index.html",
+                "breakpage": "",
+                "startpage": 0,
+                "endpage": 0,
+                "startindex": 0,
+                "endindex": 0,
+                "title": "练习"
+            }
+        ]
+    },
+    "Message": ""
+};
