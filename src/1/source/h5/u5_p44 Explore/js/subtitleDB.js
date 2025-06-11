3aaf0b24e24a613476e61a97a0950f5b3fea60c8
gzselian <3441775508@qq.com>
2025-06-11 18:00:59 +0800

    update src file from pakeplus

---
+++ src/1/source/h5/u5_p44 Explore/js/subtitleDB.js
@@ -0,0 +1,138 @@
+var movPath="../../mov/P44 Explore.mp4";
+var soundPath="source/01bg.mp3";
+var movRatio="16:9";
+
+var subtitleJson = {
+
+"wordChArr": [
+    "在花园里，",
+    "在树上，",
+    "有六只红色的蝴蝶。",
+    "你能看见吗？",
+	
+    "在花园里，",
+    "在树上，",
+    "有五只绿蜥蜴。",
+    "你能看见吗？",
+	
+    "在花园里，",
+    "在树上，",
+    "有四只棕色的鸟。",
+    "你能看见吗？",
+	
+    "在花园里，",
+    "在树上，",
+    "有三只紫色的猫。",
+    "你能看见吗？",
+	
+    "在花园里，",
+    "在树上，",
+    "有两只黄色的狗。",
+    "你能看见吗？",
+	
+    "现在有一个问题。",
+    "看那棵树！",
+    "有二十只动物！",
+    "你能看见吗？",
+	
+    "——来自塔姆辛·汤普森",
+	
+	"天空是蓝色的。",
+	"云是白色的。",
+	"它们看起来都是黑色的",
+	"在漆黑，漆黑的夜晚。",
+	"——来自莱斯莉·库斯塔夫"
+	
+],
+"wordArr": [
+    "Out in the garden, ",
+    "Up in the tree, ",
+    "There are six red butterflies.",
+    "Can you see?",
+	
+    "Out in the garden, ",
+    "Up in the tree, ",
+    "There are five green lizards.",
+    "Can you see?",
+	
+    "Out in the garden, ",
+    "Up in the tree, ",
+    "There are four brown birds. ",
+    "Can you see?",
+	
+    "Out in the garden, ",
+    "Up in the tree, ",
+    "There are three purple cats. ",
+    "Can you see?",
+	
+    "Out in the garden, ",
+    "Up in the tree, ",
+    "There are two yellow dogs. ",
+    "Can you see?",
+	
+    "Now there’s a problem.",
+    "Look at the tree! ",
+    "There are twenty animals!",
+    "Can you see?",
+	
+    "by Tamzin Thompson",
+	
+	"The sky is blue. ",
+	"The clouds are white.",
+	"They all look black",
+	"In the dark, dark night.",
+	"by Lesley Koustaff"
+],
+"timeArr": [
+"4916", "7833",
+"7875", "10083",
+"10125", "14208",
+"14250", "16250",
+"16875", "19916",
+"19958", "22500",
+"22541", "26708",
+"26750", "29041",
+"29416", "32375",
+"32416", "34958",
+"35000", "38708",
+"38750", "41333",
+"41875", "44916",
+"44958", "47625",
+"47666", "51666",
+"51708", "54375",
+"54750", "57875",
+"57916", "60333",
+"60375", "64583",
+"64625", "66833",
+"70375", "73750",
+"74041", "77083",
+"77291", "81333",
+"81541", "83500",
+"84041", "86625",
+"92750", "95625",
+"96125", "99166",
+"100208", "102666",
+"103166", "106250",
+"106541", "108958"
+], // 30
+"rolepoints": [
+    "a", "a", "a", "a", "a", "a",
+    "a", "a", "a", "a", "a", "a",
+    "a", "a", "a", "a", "a", "a",
+    "a", "a", "a", "a", "a", "a",
+    "a", "a", "a", "a", "a", "a",
+],
+
+
+// 齐声
+// 1:05.349-1:07.919
+// 角色出现顺序
+
+"allrole":
+     ["a"]
+};
+var rolelist=["a"];
+
+var Allrolepoints = [];
+
+ 
