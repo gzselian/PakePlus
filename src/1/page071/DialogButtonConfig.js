46143772a6a385993f91b0c6779c5e86c8258d5a
gzselian <3441775508@qq.com>
2025-06-11 15:01:41 +0800

    update src file from pakeplus

---
+++ src/1/page071/DialogButtonConfig.js
@@ -0,0 +1,44 @@
+
+var page071 = {
+    "Success": true,
+    "data": {
+        "pageContents": [
+            {
+                "contentId": 8888,
+                "sortNum": "1",
+                "x": "215.7",
+                "y": "109.2",
+                "width": "318",
+                "height": "50",
+                "audio": "source/sound/p71 What do you do on.mp3"
+            },
+            {
+                "contentId": 8888,
+                "sortNum": "2",
+                "x": "263.8",
+                "y": "159.2",
+                "width": "281",
+                "height": "50",
+                "audio": "source/sound/p71 What do you do on.mp3"
+            }
+        ],
+        "buttons": [
+		{
+				"id": "0",
+				"sourceType": "92",
+				"x": "680",
+				"y": "880",
+				"width": "60",
+				"height": "60",
+				"fileUrl": "source/mov/daor/u8_daor.mp4",
+				// "breakpage":"33,6",
+				"startpage": 0,
+				"endpage": 0,
+				"startindex": 0,
+				"endindex": 0,
+				"title": "导入视频"
+			}
+        ]
+    },
+    "Message": ""
+};
