10c372c707659b3c597bf1a0fbc5b6170a5161f6
gzselian <3441775508@qq.com>
2025-06-11 19:12:55 +0800

    update src file from pakeplus

---
+++ src/1/source/h5/u8_p80 Extend/js/subtitleDB.js
@@ -0,0 +1,86 @@
+var movPath="../../mov/P80 Extend.mp4";
+// var movPath="source/08.mp4";
+var soundPath="source/01bg.mp3";
+var movRatio="16:9";
+
+var subtitleJson = {
+"wordChArr": [
+    "张乐来自中国。",
+    "儿童节在6月1日。",
+    "（这一天）没有课。",
+    "他和他的同学可以参观博物馆",
+    "或去公园。",
+    "Ariya来自泰国。",
+    "儿童节在一月的第二个星期六。",
+    "Ariya喜欢去动物园！",
+    "她在那里过得很愉快。",
+    "在日本，",
+	"女孩的儿童节是在3月3日。",
+    "男孩的儿童节则是在5月5日。",
+    "Akio是一个男孩。",
+    "在儿童节，",
+    "他的家人在房子外面放飞纸鱼。",
+    "有一条纸鱼是给他爸爸的，",
+    "一条给他妈妈，",
+    "一条给他哥哥，还有一条是给Akio的！"
+],
+"wordArr": [
+    "Zhang Le is from China.",
+    "Children’s Day is on 1st June.",
+    "There are no classes.",
+    "He and his classmates can visit museums",
+    "or go to parks.",
+    "Ariya is from Thailand." ,
+    "Children’s Day is on the second Saturday in January. ",
+    "Ariya likes to go to the zoo!",
+    "She has a great time there.",
+    "In Japan," ,
+	"Children’s Day for girls is on 3rd March." ,
+    "For boys, it is on 5th May. ",
+    "Akio is a boy.",
+    "On Children’s Day,",
+    "his family flies paper fish outside their house.",
+    "There is one fish for his father,",
+    "one for his mother,",
+    "one for his brother and one for Akio!"
+],
+"timeArr": [
+"5766", "8833",
+"9066", "13333",
+"13533", "16433",
+"16566", "21133",
+"21200", "23066",
+"24166", "27333",
+"27766", "33266",
+"33600", "36900",
+"37366", "40566",
+"41766", "43000",
+"43003", "48366",
+"48600", "52900",
+"53400", "56200",
+"56600", "58600",
+"58766", "64533",
+"64666", "68466",
+"68833", "70800",
+"71033", "75733"
+], // 18
+
+// 齐声
+// 1:05.349-1:07.919
+// 角色出现顺序
+"rolepoints": [
+    "rolr",  "rolr", "rolr", "rolr", "rolr", "rolr",
+    "rolr",  "rolr", "rolr", "rolr", "rolr", "rolr",
+    "rolr",  "rolr", "rolr", "rolr", "rolr", "rolr",
+	
+
+
+	 ],
+"allrole":
+     ["rolr"]
+};
+var rolelist=["rolr"];
+
+var Allrolepoints = [];
+
+ 
