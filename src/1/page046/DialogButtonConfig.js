fa3af7108a922c0da7b68dcdd0493b785fe17708
gzselian <3441775508@qq.com>
2025-06-11 14:56:52 +0800

    update src file from pakeplus

---
+++ src/1/page046/DialogButtonConfig.js
@@ -0,0 +1,41 @@
+
+var page046 = {
+    "Success": true,
+    "data": {
+        "pageContents": [
+        ],
+        "buttons": [
+            {
+                "id": "1",
+                "sourceType": "93",
+                "x": "471.75",
+                "y": "66",
+                "width": "33",
+                "height": "33",
+                "fileUrl": "source/h5/u5_p46 A Which poem do you like Tick and say/index.html",
+                "breakpage": "",
+                "startpage": 0,
+                "endpage": 0,
+                "startindex": 0,
+                "endindex": 0,
+                "title": "课件"
+            },
+            {
+                "id": "2",
+                "sourceType": "94",
+                "x": "263.75",
+                "y": "475",
+                "width": "33",
+                "height": "33",
+                "fileUrl": "source/h5/u5_p46 B Read and match/index.html",
+                "breakpage": "",
+                "startpage": 0,
+                "endpage": 0,
+                "startindex": 0,
+                "endindex": 0,
+                "title": "练习"
+            }
+        ]
+    },
+    "Message": ""
+};
