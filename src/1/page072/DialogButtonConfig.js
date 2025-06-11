2b8fc2a15b75676b5edeed1b085078bc917425e6
gzselian <3441775508@qq.com>
2025-06-11 15:01:52 +0800

    update src file from pakeplus

---
+++ src/1/page072/DialogButtonConfig.js
@@ -0,0 +1,119 @@
+
+var page072 = {
+    "Success": true,
+    "data": {
+        "pageContents": [
+            {
+                "contentId": 8888,
+                "sortNum": "1",
+                "x": "91.25",
+                "y": "323.65",
+                "width": "160.95",
+                "height": "36",
+                "audio": "source/sound/go to the beach.mp3"
+            },
+            {
+                "contentId": 8888,
+                "sortNum": "2",
+                "x": "299.4",
+                "y": "313.6",
+                "width": "171",
+                "height": "36",
+                "audio": "source/sound/go to the cinema.mp3"
+            },
+            {
+                "contentId": 8888,
+                "sortNum": "3",
+                "x": "493.35",
+                "y": "382.6",
+                "width": "178.95",
+                "height": "36",
+                "audio": "source/sound/go to the museum.mp3"
+            },
+            {
+                "contentId": 8888,
+                "sortNum": "4",
+                "x": "98.3",
+                "y": "493.6",
+                "width": "146",
+                "height": "36",
+                "audio": "source/sound/go to the park.mp3"
+            },
+            {
+                "contentId": 8888,
+                "sortNum": "5",
+                "x": "329.65",
+                "y": "497.6",
+                "width": "138.95",
+                "height": "36",
+                "audio": "source/sound/go to the zoo.mp3"
+            },
+			{
+			    "contentId": 8888,
+			    "sortNum": "99",
+			    "x": "145.2",
+			    "y": "36.8",
+			    "width": "148",
+			    "height": "38",
+			    "audio": "source/title/Getready.mp3"
+			}
+			,
+			{"contentId": 8888,
+                "sortNum": "1",
+                "x": "52.3",
+                "y": "98.95",
+                "width": "300",
+                "height": "36",
+                "audio": "source/title/A Listen Then point and say.mp3"
+            }
+        ],
+        "buttons": [
+            {
+                "id": "1",
+                "sourceType": "11",
+                "x": "352.75",
+                "y": "101.9",
+                "width": "33",
+                "height": "33",
+                "fileUrl": "source/sound/p72 Listen_Then point and say.mp3",
+                "breakpage": "",
+                "startpage": 0,
+                "endpage": 0,
+                "startindex": 0,
+                "endindex": 0,
+                "title": "课文录音"
+            },
+            {
+                "id": "2",
+                "sourceType": "93",
+                "x": "238.3",
+                "y": "559.85",
+                "width": "33",
+                "height": "33",
+                "fileUrl": "source/h5/u8_p72 B Write and talk/index.html",
+                "breakpage": "",
+                "startpage": 0,
+                "endpage": 0,
+                "startindex": 0,
+                "endindex": 0,
+                "title": "课件"
+            },
+            {
+                "id": "3",
+                "sourceType": "93",
+                "x": "379.75",
+                "y": "101.9",
+                "width": "33",
+                "height": "33",
+                 "fileUrl": "source/h5/u8_p72 A Listen_Then point and say/index.html",
+                "breakpage": "",
+                "startpage": 0,
+                "endpage": 0,
+                "startindex": 0,
+                "endindex": 0,
+                "title": "课件"
+            }
+        ]
+    },
+    "Message": ""
+};
