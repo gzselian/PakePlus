62ef201b155c41d4bf69ddcb434485c770175f0a
gzselian <3441775508@qq.com>
2025-06-11 14:58:55 +0800

    update src file from pakeplus

---
+++ src/1/page056/DialogButtonConfig.js
@@ -0,0 +1,41 @@
+
+var page056 = {
+    "Success": true,
+    "data": {
+        "pageContents": [
+        ],
+        "buttons": [
+            {
+                "id": "1",
+                "sourceType": "93",
+                "x": "476.75",
+                "y": "62.9",
+                "width": "33",
+                "height": "33",
+                "fileUrl": "source/h5/u6_p56 A Whose home do you like Tick and say/index.html",
+                "breakpage": "",
+                "startpage": 0,
+                "endpage": 0,
+                "startindex": 0,
+                "endindex": 0,
+                "title": "课件"
+            },
+            {
+                "id": "2",
+                "sourceType": "94",
+                "x": "263.75",
+                "y": "471.85",
+                "width": "33",
+                "height": "33",
+                "fileUrl": "source/h5/u6_p56 B Read and match/index.html",
+                "breakpage": "",
+                "startpage": 0,
+                "endpage": 0,
+                "startindex": 0,
+                "endindex": 0,
+                "title": "练习"
+            }
+        ]
+    },
+    "Message": ""
+};
