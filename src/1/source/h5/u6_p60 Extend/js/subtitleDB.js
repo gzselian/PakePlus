de54a291e7c6f943e54f6f5bcac0a8fa55656326
gzselian <3441775508@qq.com>
2025-06-11 18:33:15 +0800

    update src file from pakeplus

---
+++ src/1/source/h5/u6_p60 Extend/js/subtitleDB.js
@@ -0,0 +1,82 @@
+var movPath="../../mov/P60 Extend.mp4";
+var soundPath="source/01bg.mp3";
+var movRatio="16:9";
+
+var subtitleJson = {
+"wordChArr": [
+    "世界各地有着各种各样的住宅。",
+    "其中一些非常特别。",
+	
+    "我来自北极。",
+    "我的家是用雪建造的，但里面很暖和。",
+	
+    "我来自蒙古。",
+    "我们四处迁徙。",
+    "我们喜欢我们的家！",
+	
+    "我来自英国。",
+    "我和姐姐有一间树屋！",
+    "我们在那里玩得很开心！",
+	
+    "我来自中国。",
+    "我家不在街道上。",
+    "它在水边！",
+    "它很漂亮。"
+],
+"wordArr": [
+    "There are many kinds of homes around the world.",
+    "Some of them are very special.",
+	
+    "I’m from the Arctic. ",
+    "My home is made of snow, but it’s warm.",
+	
+    "I’m from Mongolia.",
+    "We move from place to place.",
+    "We like our home!",
+	
+    "I’m from the UK.",
+    "My sister and I have a tree house! ",
+    "We have so much fun there!",
+	
+    "I’m from China.",
+    "My home isn’t on a street.",
+    "It’s by the water!",
+    "It’s beautiful."
+],
+"timeArr": [
+"5066", "9866",
+"10200", "13233",
+"13933", "16500",
+"16900", "22266",
+"22900", "25300",
+
+"25600", "28933",
+"29233", "31900",
+"32600", "35333",
+"35800", "40566",
+"40866", "44633",
+
+"45366", "47166",
+"47600", "50433",
+"50666", "52566",
+"52733", "54566"
+], // 14
+"rolepoints": [
+    "a", "a", 
+	"b", "b", 
+	"c", "c","c",
+	"d", "d", "d", 
+	"e", "e","e", "e"
+],
+
+// 齐声
+// 1:05.349-1:07.919
+// 角色出现顺序
+"allrole":
+     [ "a","b","c","d","e"]
+};
+var rolelist=[ "a","b","c","d","e"];
+
+var Allrolepoints = [];
+
+ 
