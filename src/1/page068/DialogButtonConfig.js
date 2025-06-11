c35ea280924c199888378e5604a3b0639ec5ac4c
gzselian <3441775508@qq.com>
2025-06-11 15:01:12 +0800

    update src file from pakeplus

---
+++ src/1/page068/DialogButtonConfig.js
@@ -0,0 +1,74 @@
+
+var page068 = {
+    "Success": true,
+    "data": {
+        "pageContents": [
+			{
+			    "contentId": 8888,
+			    "sortNum": "97",
+			    "x": "151.55",
+			    "y": "37",
+			    "width": "196",
+			    "height": "38",
+			    "audio": "source/title/Communicate.mp3"
+			},
+			{
+                "contentId": 8888,
+                "sortNum": "1",
+                "x": "59.95",
+                "y": "111.9",
+                "width": "284",
+                "height": "32",
+                "audio": "source/title/Listen and tick then say.mp3"
+            }
+        ],
+        "buttons": [
+            {
+                "id": "1",
+                "sourceType": "11",
+                "x": "340.75",
+                "y": "109.9",
+                "width": "33",
+                "height": "33",
+                "fileUrl": "source/sound/p68 A Listen and tick_Then say.mp3",
+                "breakpage": "",
+                "startpage": 0,
+                "endpage": 0,
+                "startindex": 0,
+                "endindex": 0,
+                "title": "课文录音"
+            },
+            {
+                "id": "2",
+                "sourceType": "93",
+                "x": "396.75",
+                "y": "109.9",
+                "width": "33",
+                "height": "33",
+                "fileUrl": "source/h5/u7_p68 A Listen and tick_Then say02/index.html",
+                "breakpage": "",
+                "startpage": 0,
+                "endpage": 0,
+                "startindex": 0,
+                "endindex": 0,
+                "title": "课件"
+            },
+            {
+                "id": "3",
+                "sourceType": "94",
+                "x": "369.75",
+                "y": "109.9",
+                "width": "33",
+                "height": "33",
+                "fileUrl": "source/h5/u7_p68 A Listen and tick_Then say/index.html",
+                "breakpage": "",
+                "startpage": 0,
+                "endpage": 0,
+                "startindex": 0,
+                "endindex": 0,
+                "title": "练习"
+            }
+        ]
+    },
+    "Message": ""
+};
