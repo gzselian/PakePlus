3bd71213c24a74c4b797566369da9f5ce651279b
gzselian <3441775508@qq.com>
2025-06-11 15:06:48 +0800

    update src file from pakeplus

---
+++ src/1/source/h5/u1_p10 Extend/js/subtitleDB.js
@@ -0,0 +1,124 @@
+var movPath="../../mov/P10 Extend.mp4";
+var soundPath="source/01bg.mp3";
+var movRatio="16:9";
+
+var subtitleJson = {
+"wordChArr": [
+    "天气又暖和又晴朗！",
+	"我喜欢睡觉。",
+	"本，你喜欢做什么？",
+	
+	"我喜欢爬树。",
+	
+	"天气炎热。",
+	"我喜欢跳舞。",
+	"来跳舞吧！",
+	
+	"对不起，鲍勃。",
+	"我有工作要做。",
+	
+	"天气凉爽。",
+	"我喜欢唱歌。",
+	"过来，本！",
+	
+	"不！我得去弄点食物。",
+	
+	"外面很冷。",
+	"我喜欢在家看书。 ",
+	"这很舒适，而且很暖和。",
+	
+	"太冷了。",
+	"我没有食物。 ",
+	"我没有家。",
+	
+	"请进！",
+	"你可以分享我的食物。",
+	
+	"谢谢你！",
+	"明年我会准备食物。"
+],
+"wordArr": [
+    "It’s warm and sunny!",
+	"I like sleeping.",
+	"What do you like doing, Ben?",
+	
+	"I like climbing trees.",
+	
+	"It’s hot.",
+	"I like dancing.",
+	"Come and dance!",
+	
+	"Sorry, Bob.",
+	"I have work to do.",
+	
+	"The weather is cool. ",
+	"I like singing. ",
+	"Come here, Ben! ",
+	
+	"No. I have to get some food.",
+	
+	"It’s cold outside.",
+	"I like reading at home. ",
+	"It’s nice and warm.",
+	
+	"It’s so cold. ",
+	"I have no food. ",
+	"I have no home.",
+	
+	"Come in!",
+	"You can share my food.",
+	
+	"Thank you!",
+	"I’ll get food next year."
+	],
+
+"timeArr": [
+"6900", "10100",
+"10400", "13166",
+"13500", "17500",
+"18400", "21933",
+"25000", "26966",
+"27366", "30100",
+"30433", "33200",
+"33333", "34966",
+"35100", "38233",
+"42600", "45400",
+"45700", "48300",
+"48566", "51200",
+"52166", "57400",
+"58666", "61866",
+"62000", "65533",
+"65633", "68966",
+"69733", "72666",
+"72966", "75966",
+"76300", "79500",
+"80533", "82533",
+"82633", "85733",
+"85966", "87666",
+"87866", "91666"
+] // 23
+
+,
+// 齐声
+// 1:05.349-1:07.919
+// 角色出现顺序
+"rolepoints":
+     ["s1","s1","s1",
+	 "s2",
+	 "s1","s1","s1",
+	 "s2","s2",
+	 "s1","s1","s1",
+	 "s2",
+	 "s2","s2","s2",
+	 "s1","s1","s1",
+	 "s2","s2",
+	 "s1","s1"
+	 ],
+"allrole":
+     ["s1","s2"]
+};
+var rolelist=["s1","s2"];
+
+var Allrolepoints = [];
+
+ 
