716c584e312d869dd35d30d8f1ecf419a1b727e0
gzselian <3441775508@qq.com>
2025-06-11 18:53:22 +0800

    update src file from pakeplus

---
+++ src/1/source/h5/u7_p70 Extend/js/subtitleDB.js
@@ -0,0 +1,71 @@
+var movPath="../../mov/P70 Extend.mp4";
+//var movPath="source/07.mp4";
+var soundPath="source/01bg.mp3";
+var movRatio="16:9";
+
+var subtitleJson = {
+"wordChArr": [
+    "学校生活非常丰富多彩。",
+    "让我们来认识一下来自世界各地的学生。",
+    "他们在学校里做些什么呢？",
+	"杨婷来自中国。",
+    "她在上剪纸课。",
+    "她玩得很开心!",
+    "泰勒来自英国。",
+    "她在学校的花园里。",
+    "她和她的朋友喜欢在花园里帮忙。",
+    "马修来自澳大利亚。",
+    "他在学校乐队里。",
+    "马修打鼓。",
+    "他和他的朋友们热爱音乐。",
+    
+],
+"wordArr": [
+    "School life is very colourful.",
+    "Let’s meet some students from around the world.",
+    "What do they do in their schools?",
+    "Yang Ting is from China.",
+    "She is in a paper-cutting class.",
+    "She has lots of fun!",
+    "Taylor is from the UK.",
+    "She is in the school garden.",
+    "She and her friend like to help in the garden.",
+    "Matthew is from Australia.",
+    "He is in the school band.",
+    "Matthew plays the drum.",
+    "He and his friends love music."
+],
+"timeArr": [
+"5800", "9533",
+"9966", "14366",
+"14766", "18466",
+"19133", "22566",
+"23033", "26833",
+
+"27033", "29833",
+"30433", "33800",
+"34033", "37533",
+"37766", "42866",
+"43566", "47000",
+
+"47333", "50466",
+"50833", "53600",
+"53966", "58200"
+], // 13
+
+// 齐声
+// 1:05.349-1:07.919
+// 角色出现顺序
+"rolepoints": [
+    "rolr",  "rolr", "rolr", "rolr", "rolr", "rolr",
+    "rolr",  "rolr", "rolr", "rolr", "rolr", "rolr",
+    "rolr"
+],
+"allrole":
+     ["rolr"]
+};
+var rolelist=["rolr"];
+
+var Allrolepoints = [];
+
+ 
