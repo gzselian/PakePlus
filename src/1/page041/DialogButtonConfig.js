e74d28fbabf4fdab4cbf92bb22685d31f973e434
gzselian <3441775508@qq.com>
2025-06-11 14:55:58 +0800

    update src file from pakeplus

---
+++ src/1/page041/DialogButtonConfig.js
@@ -0,0 +1,34 @@
+var page041 = {
+    "Success": true,
+    "data": {
+        "pageContents": [
+            {
+                "contentId": 8888,
+                "sortNum": "1",
+                "x": "92.45",
+                "y": "114.6",
+                "width": "440",
+                "height": "50",
+                "audio": "source/sound/p41 Where can we see colours.mp3"
+            }
+        ],
+        "buttons": [
+		{
+				"id": "0",
+				"sourceType": "92",
+				"x": "680",
+				"y": "890",
+				"width": "60",
+				"height": "60",
+				"fileUrl": "source/mov/daor/u5_daor.mp4",
+				// "breakpage":"33,6",
+				"startpage": 0,
+				"endpage": 0,
+				"startindex": 0,
+				"endindex": 0,
+				"title": "导入视频"
+			}
+        ]
+    },
+    "Message": ""
+};
