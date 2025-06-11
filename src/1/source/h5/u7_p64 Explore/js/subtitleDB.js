9f5f2998255b7f40fa97d9b196b52f8701b51430
gzselian <3441775508@qq.com>
2025-06-11 18:40:57 +0800

    update src file from pakeplus

---
+++ src/1/source/h5/u7_p64 Explore/js/subtitleDB.js
@@ -0,0 +1,119 @@
+var movPath="../../mov/P64 Explore.mp4";
+// var movPath="source/07.mp4";
+var soundPath="source/01bg.mp3";
+var movRatio="16:9";
+
+var subtitleJson = {
+"wordChArr": [
+    "欢迎你，妮妮！",
+    "我的名字叫兰兰。",
+    "我可以带你参观学校。",
+	"跟我来！",
+    "这是我们的教室。",
+    "看！这是我的画。",
+    "画得很好。",
+    "我喜欢在美术室画画！",
+    "我喜欢打篮球和游泳。",
+    "我们学校有游泳池吗？",
+    "没有。",
+    "但是有一个运动场。",
+    "我们走吧！",
+    "嗨，浩浩！",
+    "这是妮妮。",
+    "她也喜欢打篮球。",
+    "明天过来玩吧。",
+    "谢谢！",
+    "兰兰，这里也有图书馆吗？",
+    "来看看吧！",
+    "看！这个盒子里有新书。",
+    "哦！我喜欢这个故事！",
+    "它是关于什么的？",
+    "它是关于一个女孩的。",
+    "她刚转学到一所新学校。",
+    "她玩得很开心！",
+    
+],
+"wordArr": [
+    "Welcome, Nini!",
+    "My name is Lanlan. ",
+    "I can show you around the school.",
+    "Come with me!",
+    "This is our classroom.",
+    "Look! This is my picture.",
+    "It’s very good.",
+    "I like painting in the art room!",
+    "I like playing basketball and swimming. ",
+    "Is there a swimming pool in our school?",
+    "No, there isn’t.",
+    "But there’s a sports ground. ",
+    "Let’s go!",
+    "Hi, Haohao!",
+    "This is Nini. ",
+    "She likes playing basketball too.",
+    "Come and play tomorrow.",
+    "Thank you!",
+    "Lanlan, is there also a library?",
+    "Come and see!",
+    "Look! There are new books in this box.",
+    "Oh! I love this story!",
+    "What is it about?",
+    "It’s about a girl.",
+    "She starts in a new school.",
+    "She has lots of fun!"
+],
+"timeArr": [
+"5466", "7600",
+"7733", "10000",
+"10433", "13866",
+"14100", "16300",
+"19700", "22633",
+"22800", "26733",
+"26866", "29033",
+"29233", "33400",
+"34000", "38366",
+"39300", "42866",
+
+"43100", "44966",
+"45200", "48100",
+"48233", "50166",
+"56333", "58100",
+"58300", "60166",
+"60333", "63933",
+"64300", "66866",
+"67133", "68633",
+"68766", "72533",
+"72766", "74800",
+
+"77666", "82866",
+"83033", "86166",
+"86433", "88800",
+"89000", "91300",
+"91466", "94233",
+"94400", "97600"
+], // 26
+
+// 齐声
+// 1:05.349-1:07.919
+// 角色出现顺序
+"rolepoints": [
+    "Fanfan", "Fanfan", "Fanfan", "Fanfan", "Fanfan", "Fanfan",
+    "Nin", "Fanfan","Nin",  "Nin", "Fanfan", "Fanfan", "Fanfan",
+    "Fanfan", "Fanfan", "Fanfan", "Haohao", "Nin", "Nin",
+    "Fanfan", "Fanfan", "Nin", "Fanfan", "Nin", "Nin",
+    "Nin",
+],
+// "rolepoints": [
+//     "Fanfan", "Fanfan", "Fanfan", "Fanfan", "Fanfan", "Fanfan",
+//     "Nin", "Fanfan","Fanfan",  "Nin", "Fanfan", "Fanfan", "Fanfan",
+//     "Fanfan", "Fanfan", "Fanfan", "Haohao", "Nin", "Nin",
+//     "Fanfan", "Fanfan", "Nin", "Fanfan", "Nin", "Nin",
+//     "Nin",
+// ],
+"allrole":
+     [ "Fanfan", "Nin", "Haohao"]
+};
+var rolelist=["Teacher","Xiaoyuan","Leilei","Xiaolan","Congcong"];
+
+var Allrolepoints = [];
+
+ 
