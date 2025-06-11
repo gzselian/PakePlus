60b6eba584288b1af521eed6d1d6009b05d1c3f6
gzselian <3441775508@qq.com>
2025-06-11 14:52:33 +0800

    update src file from pakeplus

---
+++ src/1/page023/DialogButtonConfig.js
@@ -0,0 +1,65 @@
+
+var page023 = {
+    "Success": true,
+    "data": {
+        "pageContents": [
+		{
+                "contentId": 8888,
+                "sortNum": "1",
+                "x": "74.75",
+                "y": "204.95",
+                "width": "253",
+                "height": "36",
+                 "audio": "source/title/p23 Listen and write.mp3"
+            }
+        ],
+        "buttons": [
+            {
+                "id": "1",
+                "sourceType": "11",
+                "x": "326.7",
+                "y": "203.85",
+                "width": "33",
+                "height": "33",
+                "fileUrl": "source/sound/p23 C Listen and write.mp3",
+                "breakpage": "",
+                "startpage": 0,
+                "endpage": 0,
+                "startindex": 0,
+                "endindex": 0,
+                "title": "课文录音"
+            },
+            /* {
+                "id": "2",
+                "sourceType": "93",
+                "x": "382.7",
+                "y": "203.85",
+                "width": "33",
+                "height": "33",
+                "fileUrl": "source/h5/u3_p23 C Listen_ number and write02/index.html",
+                "breakpage": "",
+                "startpage": 0,
+                "endpage": 0,
+                "startindex": 0,
+                "endindex": 0,
+                "title": "课件"
+            }, */
+            {
+                "id": "3",
+                "sourceType": "94",
+                "x": "354.7",
+                "y": "203.85",
+                "width": "33",
+                "height": "33",
+                "fileUrl": "source/h5/u3_p23 C Listen_ number and write/index.html",
+                "breakpage": "",
+                "startpage": 0,
+                "endpage": 0,
+                "startindex": 0,
+                "endindex": 0,
+                "title": "练习"
+            }
+        ]
+    },
+    "Message": ""
+};
