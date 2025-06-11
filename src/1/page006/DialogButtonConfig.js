75cf65dd3b09389fd5f3b5543bbb09961b714f07
gzselian <3441775508@qq.com>
2025-06-11 14:48:27 +0800

    update src file from pakeplus

---
+++ src/1/page006/DialogButtonConfig.js
@@ -0,0 +1,38 @@
+var page006 = {
+    "Success": true,
+    "data": {
+        "pageContents": [],
+        "buttons": [
+			{
+				"id": "1",
+				"sourceType": "94",
+				"x": "259",
+				"y": "63.7",
+				"width": "33",
+				"height": "33",
+				"fileUrl": "source/h5/u1_p6 A Read and match/index.html",
+				"startpage": 0,
+				"endpage": 0,
+				"startindex": 0,
+				"endindex": 0,
+				"title": "练习"
+			},
+			{
+                "id": "1",
+                "sourceType": "93",
+                "x": "531.75",
+                "y": "480.85",
+                "width": "33",
+                "height": "33",
+                "fileUrl": "source/h5/u1_p6 B Tick and say/index.html",
+                "breakpage": "",
+                "startpage": 0,
+                "endpage": 0,
+                "startindex": 0,
+                "endindex": 0,
+                "title": "练习"
+            }
+        ],
+    },
+    "Message": ""
+};
