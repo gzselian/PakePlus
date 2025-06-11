e37ecf6a7f264bbd79d33d1dc5a94302dd3c2990
gzselian <3441775508@qq.com>
2025-06-11 14:56:17 +0800

    update src file from pakeplus

---
+++ src/1/page043/DialogButtonConfig.js
@@ -0,0 +1,26 @@
+
+var page043 = {
+    "Success": true,
+    "data": {
+        "pageContents": [
+        ],
+        "buttons": [
+            {
+                "id": "1",
+                "sourceType": "93",
+                "x": "194.75",
+                "y": "203.9",
+                "width": "33",
+                "height": "33",
+                "fileUrl": "source/h5/u5_p43 C Tick and talk/index.html",
+                "breakpage": "",
+                "startpage": 0,
+                "endpage": 0,
+                "startindex": 0,
+                "endindex": 0,
+                "title": "课件"
+            }
+        ]
+    },
+    "Message": ""
+};
