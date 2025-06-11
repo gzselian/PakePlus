980650e08cecce9b9f12b1a9ee77e03fccf72013
gzselian <3441775508@qq.com>
2025-06-11 16:16:50 +0800

    update src file from pakeplus

---
+++ src/1/source/h5/u4_p40 Extend/js/subtitleDB.js
@@ -0,0 +1,71 @@
+var movPath="../../mov/P40 Extend.mp4";
+var soundPath="source/01bg.mp3";
+var movRatio="16:9";
+
+var subtitleJson = {
+"wordChArr": [
+    "听！你能听到什么？",
+    "我能听到“咕咚”！",
+    "救命！救命！咕咚来了！",
+    "它有绿色的眼睛！",
+    "快跑！快跑！咕咚来了！",
+    "它有八条腿！",
+    "救命！咕咚来了！",
+    "它有九条腿！",
+    "咕咚是什么？",
+    "它有四只绿色的眼睛和十条腿。",
+    "咕咚是什么？",
+    "它在湖里。",
+    "别担心。",
+    "看！这是一个木瓜。"
+],
+"wordArr": [
+    "Listen! What can you hear?",
+    "I can hear “Gu Dong”!",
+    "Help! Help! Gu Dong is coming!",
+    "It has green eyes!",
+    "Run! Run! Gu Dong is coming!",
+    "It has eight legs!",
+    "Help! Gu Dong is coming!",
+    "It has nine legs!",
+    "What’s Gu Dong?",
+    "It has four green eyes and ten legs.",
+    "What’s Gu Dong?",
+    "It’s in the lake.",
+    "Don’t worry. ",
+    "Look! It’s a papaya."
+],
+"timeArr": [
+"7333", "11100",
+"11200", "14233",
+"16766", "21566",
+"23833", "26633",
+"31900", "36033",
+
+"36733", "39566",
+"45766", "49233",
+"49433", "52133",
+"58433", "60466",
+"60700", "65666",
+
+"67233", "69433",
+"69533", "71733",
+"71900", "73500",
+"73666", "76700"
+], // 14
+"rolepoints": [
+    "r1", "r2", "r1", "r2", "m", "r1",
+    "f", "m", "e", "b", "e", "f",
+    "e", "e"
+],
+// 齐声
+// 1:05.349-1:07.919
+// 角色出现顺序
+"allrole":
+     [ "r1","r2","m","f", "e", "b"]
+};
+var rolelist=[ "r1", "r2","m","f", "e", "b"];
+
+var Allrolepoints = [];
+
+ 
