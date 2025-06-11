754b0dd3bd048990305f54887c1927443e592aca
gzselian <3441775508@qq.com>
2025-06-11 15:57:45 +0800

    update src file from pakeplus

---
+++ src/1/source/h5/u3_p30 Extend/js/subtitleDB.js
@@ -0,0 +1,81 @@
+var movPath="../../mov/P30 Extend.mp4";
+var soundPath="source/01bg.mp3";
+var movRatio="16:9";
+
+var subtitleJson = {
+"wordChArr": [
+    "嗨，我是晶晶！",
+    "我住在北京。",
+    "星期五是忙碌的一天。",
+    "我需要更换很多衣服。",
+	
+    "早上，我们开始上学。",
+    "我穿我的校服：一件衬衫和一条短裙。",
+    "我带着我的书包。",
+	
+    "下午，我们有体育课。",
+    "我穿上一件T恤和短裤。",
+    "我换了我的鞋。",
+    "我能跑得很快。",
+    "放学后，我在公园帮忙。",
+    "我们捡垃圾。",
+    "我们穿特别的T恤。",
+    "我忙碌的一天结束了。",
+    "该睡觉了。",
+    "我今天很累，但是很开心！"
+],
+"wordArr": [
+    "Hi, I’m Jingjing! ",
+    "I live in Beijing.",
+    "Friday is a busy day.",
+    "I need to change clothes a lot.",
+    "In the morning, we start school.",
+    "I wear my school uniform: a shirt and a skirt.",
+    "I bring my schoolbag.",
+    "In the afternoon, we have PE.",
+    "I put on a T-shirt and shorts.",
+    "I change my shoes.",
+    "I can run fast.",
+    "After school, I help in the park.",
+    "We pick up rubbish.",
+    "We wear special T-shirts.",
+    "My busy day is over. ",
+    "It’s time to go to bed.",
+    "I’m tired but happy today!"
+],
+"timeArr": [
+"4366", "6666",
+"6766", "8600",
+"8733", "10633",
+"10900", "13933",
+"14233", "17700",
+"18033", "24166",
+"24766", "27233",
+"27933", "32266",
+"32766", "36500",
+"36666", "39000",
+"39200", "41566",
+"41900", "45900",
+"46066", "48266",
+"48433", "51366",
+"51666", "54666",
+"54900", "57400",
+"57466", "61200"
+], // 17
+"rolepoints": [
+    "g", "g", "g", "g", "g", "g",
+    "g", "g", "g", "g", "g", "g",
+    "g", "g", "g", "g", "g"
+],
+// 齐声
+// 1:05.349-1:07.919
+// 角色出现顺序
+
+"allrole":
+     ["g"]
+};
+var rolelist=["g"];
+
+var Allrolepoints = [];
+
+ 
