070bfa421bd329c021ca1337fc0cbff3cfcd6b0b
gzselian <3441775508@qq.com>
2025-06-11 15:14:58 +0800

    update src file from pakeplus

---
+++ src/1/source/h5/u1_p4 Explore/js/subtitleDB.js
@@ -0,0 +1,93 @@
+var movPath="../../mov/P4 Explore.mp4";
+var soundPath="source/01bg.mp3";
+var movRatio="16:9";
+
+var subtitleJson = {
+"wordChArr": [
+    "你好，孩子们！",
+	"欢迎来到我们的中心！",
+	"我们来谈谈爱好吧！",
+	"你在空闲时间喜欢做什么？",
+	"把你的照片发给我们！",
+	"我喜欢音乐。",
+	"我喜欢唱歌和跳舞。",
+	"这是我在舞蹈课上。",
+	"看我的紫色裙子！",
+	"我喜欢读书。",
+	"我也喜欢画画。",
+	"这是我在我最喜欢的公园里。",
+	"我擅长运动。",
+	"我喜欢打乒乓球和骑自行车。",
+	"这是骑自行车的我。",
+	"我喜欢运动！",
+	"我经常游泳。",
+	"我也打篮球。",
+	"这是我在篮球场。"
+],
+"wordArr": [
+    "Hello, children! ",
+	"Welcome to our centre! ",
+	"Let’s talk about hobbies!",
+	"What do you like doing in your free time? ",
+	"Send us your photos!",
+	"I like music.",
+	"I like singing and dancing.",
+	"This is me in my dance class.",
+	"Look at my purple skirt!",
+	"I like reading.",
+	"I like painting too.",
+	"This is me in my favourite park.",
+	"I’m good at sport.",
+	"I like playing ping-pong and riding my bike.",
+	"This is me on my bike.",
+	"I like sport!",
+	"I swim a lot.",
+	"I play basketball too.",
+	"This is me at the basketball court."
+	],
+
+"timeArr": [
+"5366", "7166",
+"7366", "10300",
+"10600", "13833",
+"14166", "18833",
+"19166", "22500",
+
+"26000", "28566",
+"29066", "33100",
+"33500", "37666",
+"38033", "42400",
+"45266", "47433",
+
+"47833", "51100",
+"51266", "56233",
+"58600", "60933",
+"61133", "66466",
+"66633", "69666",
+
+"71800", "73800",
+"73933", "76133",
+"76366", "79033",
+"79133", "82900"
+]
+,
+// 齐声
+// 1:05.349-1:07.919
+// 角色出现顺序
+"rolepoints":
+     [
+		 "Teacher","Teacher","Teacher","Teacher","Teacher",
+		 "Xiaoyuan","Xiaoyuan","Xiaoyuan","Xiaoyuan",
+		 "Leilei", "Leilei","Leilei",
+		 "Xiaolan","Xiaolan","Xiaolan",
+		 "Congcong","Congcong","Congcong","Congcong",
+	 
+	 ],
+"allrole":
+     ["Teacher","Xiaoyuan","Leilei","Xiaolan","Congcong"]
+};
+var rolelist=["Teacher","Xiaoyuan","Leilei","Xiaolan","Congcong"];
+
+var Allrolepoints = [];
+
+ 
