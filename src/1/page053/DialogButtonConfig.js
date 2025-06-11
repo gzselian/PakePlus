a558fb477876cbfefe675e22146624e66e796f11
gzselian <3441775508@qq.com>
2025-06-11 14:58:13 +0800

    update src file from pakeplus

---
+++ src/1/page053/DialogButtonConfig.js
@@ -0,0 +1,50 @@
+
+var page053 = {
+    "Success": true,
+    "data": {
+        "pageContents": [
+		{
+                "contentId": 8888,
+                "sortNum": "1",
+                "x": "76.15",
+                "y": "202.9",
+                "width": "223",
+                "height": "32",
+                "audio": "source/title/Listen match and say.mp3"
+            }
+        ],
+        "buttons": [
+            {
+                "id": "1",
+                "sourceType": "11",
+                "x": "297.75",
+                "y": "199.9",
+                "width": "33",
+                "height": "33",
+                "fileUrl": "source/sound/p53 C Listen_match and say.mp3",
+                "breakpage": "",
+                "startpage": 0,
+                "endpage": 0,
+                "startindex": 0,
+                "endindex": 0,
+                "title": "课文录音"
+            },
+            {
+                "id": "2",
+                "sourceType": "94",
+                "x": "320.75",
+                "y": "199.9",
+                "width": "33",
+                "height": "33",
+                "fileUrl": "source/h5/u6_p53 C Listen_match and say/index.html",
+                "breakpage": "",
+                "startpage": 0,
+                "endpage": 0,
+                "startindex": 0,
+                "endindex": 0,
+                "title": "课件"
+            }
+        ]
+    },
+    "Message": ""
+};
