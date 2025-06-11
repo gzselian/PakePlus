4f2ad1f7d00401d657c1af670ee53b78d4c16d98
gzselian <3441775508@qq.com>
2025-06-11 14:54:21 +0800

    update src file from pakeplus

---
+++ src/1/page033/DialogButtonConfig.js
@@ -0,0 +1,61 @@
+var page033 = {
+	"Success": true,
+	"data": {
+		"pageContents": [{
+                "contentId": 8888,
+                "sortNum": "1",
+                "x": "76.15",
+                "y": "202.9",
+                "width": "234",
+                "height": "32",
+                "audio": "source/title/Listen number and say.mp3"
+            }],
+		"buttons": [{
+				"id": "1",
+				"sourceType": "11",
+				"x": "310.7",
+				"y": "199.85",
+				"width": "33",
+				"height": "33",
+				"fileUrl": "source/sound/p33 Listen_number and say.mp3",
+				"breakpage": "",
+				"startpage": 0,
+				"endpage": 0,
+				"startindex": 0,
+				"endindex": 0,
+				"title": "课文录音"
+			},
+			{
+				"id": "2",
+				"sourceType": "93",
+				"x": "376.7",
+				"y": "199.85",
+				"width": "33",
+				"height": "33",
+				"fileUrl": "source/h5/u4_p33 C Listen_number and say02/index.html",
+				"breakpage": "",
+				"startpage": 0,
+				"endpage": 0,
+				"startindex": 0,
+				"endindex": 0,
+				"title": "课件"
+			},
+			{
+				"id": "3",
+				"sourceType": "94",
+				"x": "343.7",
+				"y": "199.85",
+				"width": "33",
+				"height": "33",
+				"fileUrl": "source/h5/u4_p33 C Listen_number and say/index.html",
+				"breakpage": "",
+				"startpage": 0,
+				"endpage": 0,
+				"startindex": 0,
+				"endindex": 0,
+				"title": "练习"
+			}
+		]
+	},
+	"Message": ""
+};
