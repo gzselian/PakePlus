4c82a8104b612873dc074c343dc8ea1760d52fb3
gzselian <3441775508@qq.com>
2025-06-11 14:57:50 +0800

    update src file from pakeplus

---
+++ src/1/page051/DialogButtonConfig.js
@@ -0,0 +1,35 @@
+
+var page051 = {
+    "Success": true,
+    "data": {
+        "pageContents": [
+            {
+                "contentId": 8888,
+                "sortNum": "1",
+                "x": "116.8",
+                "y": "112.6",
+                "width": "412",
+                "height": "50",
+                "audio": "source/sound/p51 What are our homes like.mp3"
+            }
+        ],
+        "buttons": [
+		{
+				"id": "0",
+				"sourceType": "92",
+				"x": "680",
+				"y": "890",
+				"width": "60",
+				"height": "60",
+				"fileUrl": "source/mov/daor/u6_daor.mp4",
+				// "breakpage":"33,6",
+				"startpage": 0,
+				"endpage": 0,
+				"startindex": 0,
+				"endindex": 0,
+				"title": "导入视频"
+			}
+        ]
+    },
+    "Message": ""
+};
