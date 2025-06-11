dee0d1e15af63acd384dd28d4a2365eff6ebd74e
gzselian <3441775508@qq.com>
2025-06-11 15:02:36 +0800

    update src file from pakeplus

---
+++ src/1/page076/DialogButtonConfig.js
@@ -0,0 +1,41 @@
+
+var page076 = {
+    "Success": true,
+    "data": {
+        "pageContents": [
+        ],
+        "buttons": [
+            {
+                "id": "1",
+                "sourceType": "94",
+                "x": "267.75",
+                "y": "67.9",
+                "width": "33",
+                "height": "33",
+                "fileUrl": "source/h5/u8_p76 A Read and choose/index.html",
+                "breakpage": "",
+                "startpage": 0,
+                "endpage": 0,
+                "startindex": 0,
+                "endindex": 0,
+                "title": "课件"
+            },
+            {
+                "id": "2",
+                "sourceType": "93",
+                "x": "232.75",
+                "y": "525.85",
+                "width": "33",
+                "height": "33",
+                "fileUrl": "source/h5/u8_p76 B Read and act/index.html",
+                "breakpage": "",
+                "startpage": 0,
+                "endpage": 0,
+                "startindex": 0,
+                "endindex": 0,
+                "title": "课件"
+            }
+        ]
+    },
+    "Message": ""
+};
