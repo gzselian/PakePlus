5098faae8679e824cf2f568aef8fcd931a1ce29e
gzselian <3441775508@qq.com>
2025-06-11 14:54:13 +0800

    update src file from pakeplus

---
+++ src/1/page032/DialogButtonConfig.js
@@ -0,0 +1,134 @@
+var page032 = {
+	"Success": true,
+	"data": {
+		"pageContents": [{
+				"contentId": 8888,
+				"sortNum": "1",
+				"x": "463.8",
+				"y": "200.3",
+				"width": "54",
+				"height": "32",
+				"audio": "source/sound/bird.mp3"
+			},
+			{
+				"contentId": 8888,
+				"sortNum": "2",
+				"x": "569.5",
+				"y": "209.15",
+				"width": "54",
+				"height": "32",
+				"audio": "source/sound/duck.mp3"
+			},
+			{
+				"contentId": 8888,
+				"sortNum": "3",
+				"x": "585.85",
+				"y": "289.6",
+				"width": "54",
+				"height": "32",
+				"audio": "source/sound/lake.mp3"
+			},
+			{
+				"contentId": 8888,
+				"sortNum": "4",
+				"x": "376.5",
+				"y": "328.45",
+				"width": "54",
+				"height": "32",
+				"audio": "source/sound/hear.mp3"
+			},
+			{
+				"contentId": 8888,
+				"sortNum": "5",
+				"x": "130.4",
+				"y": "387.1",
+				"width": "54",
+				"height": "32",
+				"audio": "source/sound/grass.mp3"
+			},
+			{
+				"contentId": 8888,
+				"sortNum": "6",
+				"x": "493.1",
+				"y": "378.25",
+				"width": "54",
+				"height": "32",
+				"audio": "source/sound/bee.mp3"
+			},
+			{
+				"contentId": 8888,
+				"sortNum": "7",
+				"x": "130.4",
+				"y": "509.15",
+				"width": "54",
+				"height": "32",
+				"audio": "source/sound/river.mp3"
+			},
+			{
+			    "contentId": 8888,
+			    "sortNum": "99",
+			    "x": "145.2",
+			    "y": "36.8",
+			    "width": "148",
+			    "height": "38",
+			    "audio": "source/title/Getready.mp3"
+			}
+			,
+			{"contentId": 8888,
+                "sortNum": "1",
+                "x": "52.3",
+                "y": "98.95",
+                "width": "300",
+                "height": "36",
+                "audio": "source/title/A Listen Then point and say.mp3"
+            }
+		],
+		"buttons": [{
+				"id": "1",
+				"sourceType": "11",
+				"x": "351.9",
+				"y": "100",
+				"width": "33",
+				"height": "33",
+				"fileUrl": "source/sound/p32 Listen_Then point and say.mp3",
+				"breakpage": "",
+				"startpage": 0,
+				"endpage": 0,
+				"startindex": 0,
+				"endindex": 0,
+				"title": "课文录音"
+			},
+			{
+				"id": "2",
+				"sourceType": "93",
+				"x": "383",
+				"y": "100",
+				"width": "33",
+				"height": "33",
+				"fileUrl": "source/h5/u4_p32 A Listen_Then point and say/index.html",
+				"breakpage": "",
+				"startpage": 0,
+				"endpage": 0,
+				"startindex": 0,
+				"endindex": 0,
+				"title": "课件"
+			},
+			{
+				"id": "3",
+				"sourceType": "93",
+				"x": "233",
+				"y": "591.95",
+				"width": "33",
+				"height": "33",
+				"fileUrl": "source/h5/u4_p32 A Look and say/index.html",
+				"breakpage": "",
+				"startpage": 0,
+				"endpage": 0,
+				"startindex": 0,
+				"endindex": 0,
+				"title": "课件"
+			}
+		]
+	},
+	"Message": ""
+};
