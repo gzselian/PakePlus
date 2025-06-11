158953b9195b9b38063b20a465791cf3c67d609e
gzselian <3441775508@qq.com>
2025-06-11 15:00:07 +0800

    update src file from pakeplus

---
+++ src/1/page062/DialogButtonConfig.js
@@ -0,0 +1,127 @@
+
+var page062 = {
+    "Success": true,
+    "data": {
+        "pageContents": [
+            {
+                "contentId": 8888,
+                "sortNum": "1",
+                "x": "246.55",
+                "y": "286.65",
+                "width": "159.95",
+                "height": "36",
+                "audio": "source/sound/computer room.mp3"
+            },
+            {
+                "contentId": 8888,
+                "sortNum": "2",
+                "x": "110.3",
+                "y": "327.65",
+                "width": "108",
+                "height": "36",
+                "audio": "source/sound/classroom.mp3"
+            },
+            {
+                "contentId": 8888,
+                "sortNum": "3",
+                "x": "474.9",
+                "y": "320.65",
+                "width": "67",
+                "height": "36",
+                "audio": "source/sound/toilet.mp3"
+            },
+            {
+                "contentId": 8888,
+                "sortNum": "4",
+                "x": "87.3",
+                "y": "488.6",
+                "width": "146",
+                "height": "36",
+                "audio": "source/sound/sports ground.mp3"
+            },
+            {
+                "contentId": 8888,
+                "sortNum": "5",
+                "x": "255.75",
+                "y": "502.6",
+                "width": "83",
+                "height": "36",
+                "audio": "source/sound/library.mp3"
+            },
+            {
+                "contentId": 8888,
+                "sortNum": "6",
+                "x": "494.75",
+                "y": "502.6",
+                "width": "83",
+                "height": "36",
+                "audio": "source/sound/office.mp3"
+            },
+			{
+			    "contentId": 8888,
+			    "sortNum": "99",
+			    "x": "145.2",
+			    "y": "36.8",
+			    "width": "148",
+			    "height": "38",
+			    "audio": "source/title/Getready.mp3"
+			},
+			{"contentId": 8888,
+                "sortNum": "1",
+                "x": "52.3",
+                "y": "104.95",
+                "width": "300",
+                "height": "36",
+                "audio": "source/title/A Listen Then point and say.mp3"
+            }
+        ],
+        "buttons": [
+            {
+                "id": "1",
+                "sourceType": "11",
+                "x": "352.75",
+                "y": "112.9",
+                "width": "33",
+                "height": "33",
+                "fileUrl": "source/sound/p62 Listen_Then point and say.mp3",
+                "breakpage": "",
+                "startpage": 0,
+                "endpage": 0,
+                "startindex": 0,
+                "endindex": 0,
+                "title": "课文录音"
+            },
+            {
+                "id": "2",
+                "sourceType": "93",
+                "x": "379.75",
+                "y": "112.9",
+                "width": "33",
+                "height": "33",
+                "fileUrl": "source/h5/u7_p62 A Listen_Then point and say/index.html",
+                "breakpage": "",
+                "startpage": 0,
+                "endpage": 0,
+                "startindex": 0,
+                "endindex": 0,
+                "title": "课件"
+            },
+			{
+                "id": "1",
+                "sourceType": "94",
+                "x": "252.05",
+                "y": "554.95",
+                "width": "33",
+                "height": "33",
+                "fileUrl": "source/h5/u7_p62 B Look and write/index.html",
+                "breakpage": "",
+                "startpage": 0,
+                "endpage": 0,
+                "startindex": 0,
+                "endindex": 0,
+                "title": "未定义"
+            }
+        ]
+    },
+    "Message": ""
+};
