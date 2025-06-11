12dcbaf79b50cd10acb5bbbcdd50d7fd247bb84d
gzselian <3441775508@qq.com>
2025-06-11 14:49:35 +0800

    update src file from pakeplus

---
+++ src/1/page012/DialogButtonConfig.js
@@ -0,0 +1,101 @@
+var page012 = {
+	"Success": true,
+	"data": {
+		"pageContents": [
+			{
+				"contentId": 8888,
+				"sortNum": "0",
+				"x": "87.05",
+				"y": "311",
+				"width": "131",
+				"height": "34",
+				"audio": "page012/sound/play a game.mp3"
+			},
+			{
+				"contentId": 8888,
+				"sortNum": "1",
+				"x": "311",
+				"y": "311",
+				"width": "131",
+				"height": "34",
+				"audio": "page012/sound/take a photo.mp3"
+			},
+			{
+				"contentId": 8888,
+				"sortNum": "2",
+				"x": "538",
+				"y": "311",
+				"width": "131",
+				"height": "34",
+				"audio": "page012/sound/row a boat.mp3"
+			},
+			{
+				"contentId": 8888,
+				"sortNum": "3",
+				"x": "163.6",
+				"y": "520.95",
+				"width": "179",
+				"height": "34",
+				"audio": "page012/sound/climb a mountain.mp3"
+			},
+
+			{
+				"contentId": 8888,
+				"sortNum": "4",
+				"x": "451",
+				"y": "525.95",
+				"width": "131",
+				"height": "34",
+				"audio": "page012/sound/tell a story.mp3"
+			},
+			{
+			    "contentId": 8888,
+			    "sortNum": "99",
+			    "x": "145.2",
+			    "y": "36.8",
+			    "width": "148",
+			    "height": "38",
+			    "audio": "source/title/Getready.mp3"
+			},
+			{"contentId": 8888,
+                "sortNum": "1",
+                "x": "52.3",
+                "y": "98.95",
+                "width": "300",
+                "height": "36",
+                "audio": "source/title/A Listen Then point and say.mp3"
+            }
+		],
+		"buttons": [{
+				"id": "1",
+				"sourceType": "11",
+				"x": "351.5",
+				"y": "101.05",
+				"width": "33",
+				"height": "33",
+				"fileUrl": "source/sound/p12 A Listen_Then point and say.mp3",
+				"breakpage": "",
+				"startpage": 0,
+				"endpage": 0,
+				"startindex": 0,
+				"endindex": 0,
+				"title": "课文录音"
+			},
+			{
+				"id": "2",
+				"sourceType": "93",
+				"x": "379.5",
+				"y": "101.05",
+				"width": "33",
+				"height": "33",
+				"fileUrl": "source/h5/u2_p12 A Listen_Then point and say/index.html",
+				"startpage": 0,
+				"endpage": 0,
+				"startindex": 0,
+				"endindex": 0,
+				"title": "课件"
+			},
+		],
+	},
+	"Message": ""
+};
