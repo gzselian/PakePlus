2eece8e00f0d8975b22298751bb1c1b1c36691fe
gzselian <3441775508@qq.com>
2025-06-11 14:48:59 +0800

    update src file from pakeplus

---
+++ src/1/page009/DialogButtonConfig.js
@@ -0,0 +1,24 @@
+var page009 = {
+	"Success": true,
+	"data": {
+		"pageContents": [],
+		"buttons": [
+		{
+                "id": "1",
+                "sourceType": "93",
+                "x": "219.55",
+                "y": "67.5",
+                "width": "33",
+                "height": "33",
+                "fileUrl": "source/h5/u1_p9 B Do a survey/index.html",
+                "breakpage": "",
+                "startpage": 0,
+                "endpage": 0,
+                "startindex": 0,
+                "endindex": 0,
+                "title": "课件"
+            }
+		],
+	},
+	"Message": ""
+};
