4987d4648ba7d0b6d1d2250ec04c4991aff98b92
gzselian <3441775508@qq.com>
2025-06-11 18:21:48 +0800

    update src file from pakeplus

---
+++ src/1/source/h5/u6_p54 Explore2/js/subtitleDB.js
@@ -0,0 +1,79 @@
+var movPath="../../mov/P54 Explore2.mp4";
+var soundPath="source/01bg.mp3";
+var movRatio="16:9";
+
+var subtitleJson = {
+
+"wordChArr": [
+    "致： Panpan",
+    "来自：Lotte",
+    "主题：回复：我的新家",
+	
+    "亲爱的Panpan，",
+    "谢谢你的照片。",
+    "让我来给你看看我的家。",
+    "我家的房间都很小。",
+    "在我的卧室里，",
+    "床上有一只泰迪熊。",
+    "它是我最喜欢的玩具。",
+    "这儿没有花园。",
+    "那是因为我的家是一艘船！",
+    "你喜欢我的家吗？",
+    "最美好的祝愿，",
+    "Lotte"
+],
+"wordArr": [
+	
+    "To: Panpan",
+    "From: Lotte",
+    "Subject: RE: My new home",
+	
+    "Dear Panpan,",
+    "Thank you for your photos.",
+    "Let me show you my home.",
+    "The rooms in my home are small.",
+    "In my bedroom, ",
+    "there’s a teddy bear on the bed.",
+    "It’s my favourite toy.",
+    "There’s no garden.",
+    "That’s because my home is a boat!",
+    "Do you like my home?",
+    "Best wishes,",
+    "Lotte"
+],
+"timeArr": [
+"3791", "6125",
+"6458", "8625",
+"8833", "13750",
+"14041", "15875",
+"16125", "18500",
+"18916", "21708",
+"22291", "25833",
+"26291", "28000",
+"28041", "30708",
+"31166", "34250",
+"34708", "36750",
+"37000", "40458",
+"41041", "43125",
+"43625", "45166",
+"45416", "46750"
+], // 31
+"rolepoints": [
+	"a", "a", "a",
+	"l", "l", "l", "l", "l","l", "l", "l", "l", "l", "l", "l"
+],
+
+
+
+// 齐声
+// 1:05.349-1:07.919
+// 角色出现顺序
+
+"allrole":
+     ["a","l"]
+};
+var rolelist= ["a","l"];
+
+var Allrolepoints = [];
+
+ 
