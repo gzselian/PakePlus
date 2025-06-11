a6451371bf5b1101d4a4ee0946b309cfd645bc3e
gzselian <3441775508@qq.com>
2025-06-11 14:51:45 +0800

    update src file from pakeplus

---
+++ src/1/page021/DialogButtonConfig.js
@@ -0,0 +1,31 @@
+var page021 = {
+	"Success": true,
+	"data": {
+		"pageContents": [{
+			"contentId": 8888,
+			"sortNum": "1",
+			"x": "208.1",
+			"y": "110.2",
+			"width": "324",
+			"height": "56",
+			"audio": "source/sound/p21 What do we wear.mp3"
+		}, ],
+		"buttons": [
+		{
+				"id": "0",
+				"sourceType": "92",
+				"x": "680",
+				"y": "890",
+				"width": "60",
+				"height": "60",
+				"fileUrl": "source/mov/daor/u3_daor.mp4",
+				// "breakpage":"33,6",
+				"startpage": 0,
+				"endpage": 0,
+				"startindex": 0,
+				"endindex": 0,
+				"title": "导入视频"
+			}],
+	},
+	"Message": ""
+};
