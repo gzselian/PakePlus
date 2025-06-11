a723fe8c6b733c0805339eff90322ac7a5a6a869
gzselian <3441775508@qq.com>
2025-06-11 14:46:15 +0800

    update src file from pakeplus

---
+++ src/1/catalogue.js
@@ -0,0 +1,10 @@
+var catalogue = {
+  "Message":"",
+  "Success":true,
+  "isShowPage":false,
+  "isShowSecondaries":false,
+  "data":[
+	  {"ID":1771,"Title":"Unit 1 Apple!","StartPage":0,"EndPage":88},
+  ]
+
+}
